095a9801d23a0444f65468d18e0aac02103009ff
DianYangFu <dianyangfu@gmail.com>
2022-10-12 16:50:55 +0800

    chore(politics-tracker/styles): add `/theme` for theme provider in styled-components`

---
+++ packages/politics-tracker/styles/theme/index.js
@@ -0,0 +1,20 @@
+const mediaSize = {
+  xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  xxl: 1440,
+}
+export const theme = {
+  breakpoint: {
+    xs: `@media (min-width: ${mediaSize.xs}px)`,
+    sm: `@media (min-width: ${mediaSize.sm}px)`,
+    md: `@media (min-width: ${mediaSize.md}px)`,
+    lg: `@media (min-width: ${mediaSize.lg}px)`,
+    xl: `@media (min-width: ${mediaSize.xl}px)`,
+    xxl: `@media (min-width: ${mediaSize.xxl}px)`,
+  },
+}
+
+export default theme
