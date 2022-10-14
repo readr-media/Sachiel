3e65187ef86eecd998b4f4f020d80999addbbfe3
DianYangFu <dianyangfu@gmail.com>
2022-10-14 10:58:39 +0800

    style(politics-tracker/people): show `section-body` when click `section-toggle`

--- packages/politics-tracker/components/people/section-body.js
+++ packages/politics-tracker/components/people/section-body.js
@@ -1,3 +1,38 @@
-export default function SectionBody() {
-  return <div>123</div>
+import styled from 'styled-components'
+
+const SectionBodyContainer = styled.div`
+  //adjust position for fitting place when section-toggle is pressed
+  margin-left: 10px;
+  transition: all 0.3s ease-in-out;
+  width: 100%;
+  max-height: ${
+    /**
+     * @param {Object} props
+     * @param {boolean} props.shouldShowSectionBody
+     */
+    ({ shouldShowSectionBody }) => (shouldShowSectionBody ? 'unset' : '0px')
+  };
+  min-height: ${({ shouldShowSectionBody }) =>
+    shouldShowSectionBody ? '200px' : '0px'};
+  border-color: ${({ theme }) => theme.borderColor.black};
+  border-width: 0 4px 4px;
+  background-color: ${({ theme }) => theme.backgroundColor.white};
+  * {
+    display: ${({ shouldShowSectionBody }) =>
+      shouldShowSectionBody ? 'block' : 'none'};
+  }
+`
+
+/**
+ *
+ * @param {Object} props
+ * @param {boolean} [props.isActive]
+ * @returns
+ */
+export default function SectionBody({ isActive = false }) {
+  return (
+    <SectionBodyContainer shouldShowSectionBody={isActive}>
+      <span>{`${isActive}`}</span>
+    </SectionBodyContainer>
+  )
 }
