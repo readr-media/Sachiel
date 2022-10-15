a53d1279fd5beea2000b3205a8fef5ce9f7e611a
DianYangFu <dianyangfu@gmail.com>
2022-10-15 22:48:58 +0800

    feat(politics-tracker): fetch person basic info in people page

--- packages/politics-tracker/pages/people/[id].js
+++ packages/politics-tracker/pages/people/[id].js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { ThemeProvider } from 'styled-components'
+
 import theme from '~/styles/theme'
 import Title from '~/components/people/title'
 import SectionList from '~/components/people/section-list'
+import { cmsApiUrl } from '~/constants/config'
+import { print } from 'graphql'
+import { fireGqlRequest } from '~/utils/utils'
+import FetchPeople from '~/graphql/query/people.graphql'
 
 const Main = styled.main`
   background-color: #f3f4ff;
@@ -17,10 +22,11 @@ const MOCK_PEOPLE_DATA = {
     'https://i.insider.com/5e14f8caa5ccad16ef5dc782?width=1000&format=jpeg&auto=webp',
 }
 /**
- * @property {Object} titleData
+ * @param {Object} props
+ * @param {import('../../types/person').Person} props.peopleData
  * @returns {React.ReactElement}
  */
-export default function People() {
+export default function People({ peopleData }) {
   return (
     <ThemeProvider theme={theme}>
       <Main>
@@ -29,4 +35,20 @@ export default function People() {
       </Main>
     </ThemeProvider>
   )
+}
+
+/**
+ * @async
+ * @param {import("next").GetServerSidePropsContext} context
+ * @returns {Promise<Object>}
+ */
+export async function getServerSideProps(context) {
+  const id = context?.query?.id
+  const {
+    data: { people },
+  } = await fireGqlRequest(print(FetchPeople), { Id: id }, cmsApiUrl)
+
+  return {
+    props: { peopleData: people[0] }, // will be passed to the page component as props
+  }
 }
---
+++ packages/politics-tracker/types/person.ts
@@ -0,0 +1,15 @@
+export type Person = {
+  id: string
+  name?: string
+  alternative: string
+  other_names?: string
+  gender?: string
+  biography?: string
+  birth_date_year?: number
+  birth_date_month?: number
+  birth_date_day?: number
+  death_date_year?: number
+  death_date_month?: number
+  death_date_day?: number
+  national_identity?: string
+}
