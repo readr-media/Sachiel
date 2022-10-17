cbb23a5bd184fb9c0c922551feb220ca2c8273a2
DianYangFu <dianyangfu@gmail.com>
2022-10-17 21:52:10 +0800

    feat(politics-tracker/people): add component `edit-content-item`

--- packages/politics-tracker/components/people/edit-content.js
+++ packages/politics-tracker/components/people/edit-content.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import EditContentItem from './edit-content-item'
 
 /**
  * @returns {React.ReactElement}
  */
 export default function EditContent() {
-  return <> {<div>編輯模式</div>}</>
+  return (
+    <>
+      <EditContentItem />
+      <EditContentItem />
+      <EditContentItem />
+    </>
+  )
 }
