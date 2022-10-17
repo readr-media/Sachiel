eaa5387758924c87b082d8bb8a9e977a7bd3bf7b
Dian-Yang Fu <85677992+dyfu95@users.noreply.github.com>
2022-10-17 21:09:08 +0800

    feat(politics-tracker/people): add `edit-content` and refactor `section-body-personal-file.js` (#40)
    * feat(politics-tracker): add component `edit-content.js`
    
    * refactor(politics-tracker/people): refactor components, make `edit-content` reuseable

--- packages/politics-tracker/components/people/content.js
+++ packages/politics-tracker/components/people/content.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 import ContentTitle from './content-title'
 import EditButton from './edit-button'
 import ContentItem from './content-item'
 import styled from 'styled-components'
+import EditContent from './edit-content'
 const ContentContainer = styled.div`
   padding: 0 0 20px;
 `
@@ -12,15 +13,27 @@ const ContentContainer = styled.div`
  * @param {Object} props
  * @param {string} props.title
  * @param {React.ReactElement[] | React.ReactElement} props.children
+ * @param {React.ReactElement[] | React.ReactElement} props.editContent
  * @returns {React.ReactElement}
  */
-export default function Content({ title, children }) {
+export default function Content({
+  title,
+
+  children,
+  editContent,
+}) {
+  const [shouldShowEditMode, setShouldShowEditMode] = useState(false)
+
   return (
     <ContentContainer>
       <ContentTitle title={title}>
-        <EditButton />
+        <Fragment>
+          {!shouldShowEditMode && (
+            <EditButton onClick={() => setShouldShowEditMode(true)} />
+          )}
+        </Fragment>
       </ContentTitle>
-      {children}
+      {shouldShowEditMode ? editContent : children}
     </ContentContainer>
   )
 }
---
+++ packages/politics-tracker/components/people/edit-content.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+/**
+ * @returns {React.ReactElement}
+ */
+export default function EditContent() {
+  return <> {<div>編輯模式</div>}</>
+}
