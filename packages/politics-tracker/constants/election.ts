07e535f1a5434d1be52022668bfa7f04d9637e17
Tsuki Akiba <erase2004@gmail.com>
2022-10-20 09:55:35 +0800

    feat(politics-tracker): apply data from data loader to EVC component

---
+++ packages/politics-tracker/constants/election.ts
@@ -0,0 +1,38 @@
+import type { withKeyObject } from '~/types/common'
+
+export const districtsMapping: withKeyObject<string> = {
+  臺北市: 'taipeiCity',
+  新北市: 'newTaipeiCity',
+  桃園市: 'taoyuanCity',
+  臺中市: 'taichungCity',
+  臺南市: 'tainanCity',
+  高雄市: 'kaohsiungCity',
+  新竹縣: 'hsinchuCounty',
+  苗栗縣: 'miaoliCounty',
+  彰化縣: 'changhuaCounty',
+  南投縣: 'nantouCounty',
+  雲林縣: 'yunlinCounty',
+  嘉義縣: 'chiayiCounty',
+  屏東縣: 'pingtungCounty',
+  宜蘭縣: 'yilanCounty',
+  花蓮縣: 'hualienCounty',
+  臺東縣: 'taitungCounty',
+  澎湖縣: 'penghuCounty',
+  金門縣: 'kinmenCounty',
+  連江縣: 'lienchiangCounty',
+  基隆市: 'keelungCity',
+  新竹市: 'hsinchuCity',
+  嘉義市: 'chiayiCity',
+}
+export const electionTypesMapping: withKeyObject<string> = {
+  省長: 'governorProvince',
+  省議員: 'provinceCouncilMember',
+  縣市首長: 'mayor',
+  縣市議員: 'councilMember',
+  立法委員: 'legislator',
+  國大代表: 'nationalAssembly',
+  總統: 'president',
+  鄉鎮市區長: 'townMayor',
+  '市/區民代表': 'townCouncilMember',
+  村里長: 'villageRepresentative',
+}
