603967a0287f8103cf5ffe532ce17f288ffb4373
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2022-11-08 20:56:33 +0800

    feat(politics-tracker/politic-detail): add ts of politic-detail

---
+++ packages/politics-tracker/types/politics-detail.ts
@@ -0,0 +1,43 @@
+export type PoliticDetail = {
+  id: string
+  person: Person
+  desc: string
+  content: string
+  source: string
+  status: string
+  progress: Array<Object>
+  timeline: Array<Object>
+  expertPoint: Array<Object>
+  dispute: string
+}
+
+export type Person = {
+  id: string
+  electoral_district: ElectoralDistrict
+  party: Party
+  election: Election
+  person_id: PersonId
+}
+
+export type ElectoralDistrict = {
+  id: string
+  name: string
+}
+
+export type Party = {
+  name: string
+  image: string
+}
+
+export type Election = {
+  name: string
+  type: string
+  level: string
+  status: string
+}
+
+export type PersonId = {
+  id: string
+  name: string
+  image: string
+}
