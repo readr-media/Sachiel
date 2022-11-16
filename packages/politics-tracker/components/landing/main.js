151c02dd8aeaf7dfa63604ad4e670232b94f5c54
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2022-11-16 10:47:37 +0800

    Revert "feat(politics-tracker/Landing): add Report component"
    This reverts commit 3bf0c0eac7f2a17df77de8d1c195e5408fbf2d81.

--- packages/politics-tracker/components/landing/main.js
+++ packages/politics-tracker/components/landing/main.js
@@ -12,7 +12,6 @@ import TeamIntro from '~/components/landing/credit'
 import Credit from '~/components/landing/file-download'
 import Mayor from '~/components/landing/mayor-main'
 import Councilor from '~/components/landing/council-main'
-import Report from '~/components/landing/report'
 
 const Main = styled.main`
   width: 100%;
@@ -51,12 +50,6 @@ export default function LandingMain({ propsData }) {
           propsData={propsData}
         />
         <Credit />
-        {propsData.postsWithPoliticsTrackerTag.length !== 0 && (
-          <Report
-            // @ts-ignore
-            propsData={propsData.postsWithPoliticsTrackerTag}
-          />
-        )}
         <TeamIntro />
       </Main>
       <Footer />
