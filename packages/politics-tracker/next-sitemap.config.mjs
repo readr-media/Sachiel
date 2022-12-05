9ff4fdc5de19d73a70ba1945c823d05be15ca6e8
Tsuki Akiba <>
2022-12-05 15:13:25 +0800

    refactor(politics-tracker): change dynamic sitemap path

--- packages/politics-tracker/next-sitemap.config.mjs
+++ packages/politics-tracker/next-sitemap.config.mjs
@@ -6,9 +6,9 @@ const { siteUrl } = await tsImport.load('./constants/config.ts')
 const sitemapConfig = {
   siteUrl,
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap.xml'],
+  exclude: ['/server-sitemap-1.xml'],
   robotsTxtOptions: {
-    additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
+    additionalSitemaps: [`${siteUrl}/server-sitemap-1.xml`],
   },
 }
 export default sitemapConfig
