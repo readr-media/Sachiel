9d04b2ceeccd94a62d4ec4dd8cc3f271c335699b
Tsuki Akiba <>
2022-12-05 16:41:00 +0800

    refactor(politics-tracker): split dynamic sitemap into multiple files

---
+++ packages/politics-tracker/pages/server-sitemap-3.xml/index.tsx
@@ -0,0 +1,69 @@
+// dynamic URLs for politics summary pages
+import { getServerSideSitemap } from 'next-sitemap'
+import { GetServerSideProps } from 'next'
+import { siteUrl, cmsApiUrl } from '~/constants/config'
+import { print } from 'graphql'
+import { fireGqlRequest } from '~/utils/utils'
+// @ts-ignore: no definition
+import errors from '@twreporter/errors'
+import GetPeople from '~/graphql/query/sitemap/get-people.graphql'
+import { GenericGQLData, RawPerson } from '~/types/common'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const fields = []
+
+  try {
+    const tasks = []
+    tasks.push(fireGqlRequest(print(GetPeople), undefined, cmsApiUrl))
+    const results = await Promise.allSettled(tasks)
+
+    // politic summary page
+    const resultOfPeople = results[0] as PromiseSettledResult<
+      GenericGQLData<RawPerson[], 'people'>
+    >
+    if (resultOfPeople.status === 'fulfilled') {
+      const people = resultOfPeople.value?.data?.people ?? []
+
+      for (const person of people) {
+        const personId = String(person.id)
+        const lastModified =
+          person.updatedAt ?? person.createdAt ?? new Date().toISOString()
+
+        fields.push({
+          loc: encodeURI(`${siteUrl}/politics/${personId}`),
+          lastmod: lastModified,
+        })
+      }
+    }
+  } catch (err) {
+    // All exceptions that include a stack trace will be
+    // integrated with Error Reporting.
+    // See https://cloud.google.com/run/docs/error-reporting
+    console.error(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: errors.helpers.printAll(
+          err,
+          {
+            withStack: true,
+            withPayload: true,
+          },
+          0,
+          0
+        ),
+      })
+    )
+  }
+
+  console.log(
+    JSON.stringify({
+      severity: 'DEBUG',
+      message: `There are ${fields.length} URLs about politic summary pages.`,
+    })
+  )
+
+  return getServerSideSitemap(ctx, fields)
+}
+
+// Default export to prevent next.js errors
+export default function Sitemap() {}
