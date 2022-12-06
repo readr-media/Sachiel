10ce6de774f5e8145a2975c699bdaca4c2806e82
Tsuki Akiba <erase2004@gmail.com>
2022-12-06 14:39:28 +0800

    refactor(politics-tracker): update sitemap list

--- packages/politics-tracker/next-sitemap.config.mjs
+++ packages/politics-tracker/next-sitemap.config.mjs
@@ -1,8 +1,42 @@
 import * as tsImport from 'ts-import'
 
 const { siteUrl } = await tsImport.load('./constants/config.ts')
-
 const sitemapBaseUrl = `${siteUrl}/server-sitemaps`
+
+/** @type {(string|number)[]} */
+const yearList = ['']
+const begin = 1900
+const end = 1996
+for (let year = begin; year <= end; ) {
+  yearList.push(year)
+  // usually, we group data by year with 12-year range,
+  // to keep records amount in each sitemap under 10000,
+  // we use 4-year range between 1948 and 1972
+  year += year >= 1948 && year < 1972 ? 4 : 12
+}
+yearList.push('')
+
+/**
+ * @param {string} baseUrl
+ * @param {string} group
+ * @returns {string[]}
+ */
+function getSitemapGroup(baseUrl, group) {
+  /** @type {string[]} */
+  const urlGroup = []
+  yearList.reduce((/** @type {null|string|number} */ prev, curr) => {
+    let url
+    if (prev === null) {
+      url = `${baseUrl}/${group}/null.xml`
+    } else {
+      url = `${baseUrl}/${group}/${prev}-${curr}.xml`
+    }
+    urlGroup.push(url)
+    return curr
+  }, null)
+
+  return urlGroup
+}
 
 /** @type {import('next-sitemap').IConfig} */
 const sitemapConfig = {
@@ -13,41 +47,8 @@ const sitemapConfig = {
   robotsTxtOptions: {
     additionalSitemaps: [
       `${sitemapBaseUrl}/landing-and-elections.xml`, // landing page and election pages
-      // usually, we group data by birthday_year with 12-year range,
-      // to keep records amount in each sitemap under 10000,
-      // we use 4-year range between 1948 and 1972
-      // personal pages
-      `${sitemapBaseUrl}/person/null.xml`,
-      `${sitemapBaseUrl}/person/-1912.xml`,
-      `${sitemapBaseUrl}/person/1912-1924.xml`,
-      `${sitemapBaseUrl}/person/1924-1936.xml`,
-      `${sitemapBaseUrl}/person/1936-1948.xml`,
-      `${sitemapBaseUrl}/person/1948-1952.xml`,
-      `${sitemapBaseUrl}/person/1952-1956.xml`,
-      `${sitemapBaseUrl}/person/1956-1960.xml`,
-      `${sitemapBaseUrl}/person/1960-1964.xml`,
-      `${sitemapBaseUrl}/person/1964-1968.xml`,
-      `${sitemapBaseUrl}/person/1968-1972.xml`,
-      `${sitemapBaseUrl}/person/1972-1984.xml`,
-      `${sitemapBaseUrl}/person/1984-1996.xml`,
-      `${sitemapBaseUrl}/person/1996-.xml`,
-      // politics summary pages
-      `${sitemapBaseUrl}/politics-summary/null.xml`,
-      `${sitemapBaseUrl}/politics-summary/-1912.xml`,
-      `${sitemapBaseUrl}/politics-summary/1912-1924.xml`,
-      `${sitemapBaseUrl}/politics-summary/1924-1936.xml`,
-      `${sitemapBaseUrl}/politics-summary/1936-1948.xml`,
-      `${sitemapBaseUrl}/politics-summary/1948-1952.xml`,
-      `${sitemapBaseUrl}/politics-summary/1952-1956.xml`,
-      `${sitemapBaseUrl}/politics-summary/1956-1960.xml`,
-      `${sitemapBaseUrl}/politics-summary/1960-1964.xml`,
-      `${sitemapBaseUrl}/politics-summary/1964-1968.xml`,
-      `${sitemapBaseUrl}/politics-summary/1968-1972.xml`,
-      `${sitemapBaseUrl}/politics-summary/1972-1984.xml`,
-      `${sitemapBaseUrl}/politics-summary/1984-1996.xml`,
-      `${sitemapBaseUrl}/politics-summary/1996-.xml`,
-
-      `${sitemapBaseUrl}/politics-summary.xml`, // politics summary pages
+      ...getSitemapGroup(sitemapBaseUrl, 'person'), // personal pages
+      ...getSitemapGroup(sitemapBaseUrl, 'politics-summary'), // politics summary pages
       `${sitemapBaseUrl}/politics-detail.xml`, // politic detail pages
     ],
   },
