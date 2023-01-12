8771b58da4cb3d8ae43094bbd8789e1e1ea99120
Tsuki Akiba <erase2004@gmail.com>
2023-01-12 17:10:20 +0800

    feat(readr): add /api/graphql

---
+++ packages/readr/pages/api/graphql.ts
@@ -0,0 +1,24 @@
+// This API endpoint is for client-side GraphQL request
+
+import httpProxy from 'http-proxy'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { API_ENDPOINT } from '~/constants/config'
+
+export const config = {
+  api: {
+    // Enable `externalResolver` option in Next.js
+    externalResolver: true,
+    bodyParser: false,
+  },
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return new Promise((resolve, reject) => {
+    const proxy: httpProxy = httpProxy.createProxy()
+    proxy.once('proxyRes', resolve).once('error', reject).web(req, res, {
+      changeOrigin: true,
+      target: API_ENDPOINT,
+    })
+  })
+}
