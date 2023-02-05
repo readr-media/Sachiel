c7dfd5419f40965dc68c175de8a48c9c00aad918
Tsuki Akiba <erase2004@gmail.com>
2023-02-06 00:41:15 +0800

    refactor(readr): use per-page layout instead of global layout

--- packages/readr/pages/_app.tsx
+++ packages/readr/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { ApolloProvider } from '@apollo/client'
+import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import client from '~/apollo-client'
@@ -7,15 +9,26 @@ import Layout from '~/components/layout'
 import { NormalizeStyles } from '~/components/layout/normalize-styles'
 import theme from '~/styles/theme'
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (
+    /* eslint-disable-line no-unused-vars */ page: ReactElement
+  ) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
   return (
     <>
       <NormalizeStyles />
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          {getLayout(<Component {...pageProps} />)}
         </ThemeProvider>
       </ApolloProvider>
     </>
