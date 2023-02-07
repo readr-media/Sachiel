1dd7597bc115dc58b0894e492269ac9365381cd1
Tsuki Akiba <erase2004@gmail.com>
2023-02-07 21:04:26 +0800

    feat(readr): add fonts loading script

--- packages/readr/pages/_document.tsx
+++ packages/readr/pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+import Script from 'next/script'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -30,5 +37,31 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="zh-Hant">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+          <Script
+            id="adobe-fonts"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(d) {
+          var config = {
+            kitId: 'icf3not',
+            scriptTimeout: 3000,
+            async: true
+          },
+          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document);`,
+            }}
+          />
+        </body>
+      </Html>
+    )
   }
 }
