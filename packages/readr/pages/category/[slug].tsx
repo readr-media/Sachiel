feeb0256da96f86b6e167a3e259a8100ef5765e1
Tsuki Akiba <erase2004@gmail.com>
2023-02-14 22:28:46 +0800

    feat(readr): add category page

---
+++ packages/readr/pages/category/[slug].tsx
@@ -0,0 +1,26 @@
+// under construction
+
+import { useRouter } from 'next/router'
+import type { ReactElement } from 'react'
+import styled from 'styled-components'
+
+import LayoutGeneral from '~/components/layout/layout-general'
+import type { NextPageWithLayout } from '~/pages/_app'
+
+const Article = styled.article`
+  height: 100vh;
+  line-height: 100vh;
+  text-align: center;
+`
+
+const Category: NextPageWithLayout = () => {
+  const router = useRouter()
+
+  return <Article id="post">類別頁 #{router?.query?.slug}</Article>
+}
+
+Category.getLayout = function getLayout(page: ReactElement) {
+  return <LayoutGeneral>{page}</LayoutGeneral>
+}
+
+export default Category
