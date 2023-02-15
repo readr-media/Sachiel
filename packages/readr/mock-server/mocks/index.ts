7522b3268242b1281c21f169e74634bb4cb4cdeb
Tsuki Akiba <erase2004@gmail.com>
2023-02-15 17:49:54 +0800

    feat(readr): add Category mock

--- packages/readr/mock-server/mocks/index.ts
+++ packages/readr/mock-server/mocks/index.ts
@@ -1,9 +1,11 @@
 import type { IMocks } from '@graphql-tools/mock'
 
+import { Category } from './Category'
 import { DateTime } from './DateTime'
 import { ResizedImages } from './ResizedImage'
 
 export const mocks: IMocks = {
   DateTime,
   ResizedImages,
+  Category,
 }
