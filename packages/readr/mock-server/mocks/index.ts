8122bf893ad7ddb14f4c7bdeda208abc44a74824
Tsuki Akiba <erase2004@gmail.com>
2023-02-15 17:49:15 +0800

    feat(readr): add ResizedImages mock

--- packages/readr/mock-server/mocks/index.ts
+++ packages/readr/mock-server/mocks/index.ts
@@ -1,7 +1,9 @@
 import type { IMocks } from '@graphql-tools/mock'
 
 import { DateTime } from './DateTime'
+import { ResizedImages } from './ResizedImage'
 
 export const mocks: IMocks = {
   DateTime,
+  ResizedImages,
 }
