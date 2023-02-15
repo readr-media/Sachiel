f69d9856d3e6772ab9be267ccb3cec824c03a6a7
Tsuki Akiba <erase2004@gmail.com>
2023-02-15 17:50:27 +0800

    feat(readr): add Post mock

--- packages/readr/mock-server/mocks/index.ts
+++ packages/readr/mock-server/mocks/index.ts
@@ -2,10 +2,12 @@ import type { IMocks } from '@graphql-tools/mock'
 
 import { Category } from './Category'
 import { DateTime } from './DateTime'
+import { Post } from './Post'
 import { ResizedImages } from './ResizedImage'
 
 export const mocks: IMocks = {
   DateTime,
   ResizedImages,
   Category,
+  Post,
 }
