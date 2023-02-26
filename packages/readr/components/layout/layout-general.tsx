c6018ac208920aaee1c645c91d7a675d00ed4696
Tsuki Akiba <erase2004@gmail.com>
2023-02-27 00:31:47 +0800

    refactor(readr): update LayoutGeneral style

--- packages/readr/components/layout/layout-general.tsx
+++ packages/readr/components/layout/layout-general.tsx
@@ -1,7 +1,18 @@
+import styled from 'styled-components'
+
 import { SITE_TITLE } from '~/constants/constant'
 
 import CustomHead from './custom-head'
 import HeaderGeneral from './header/header-general'
+
+const Main = styled.main`
+  min-height: 100vh;
+  padding: 64px 0 0;
+  overflow: hidden;
+  ${({ theme }) => theme.breakpoint.sm} {
+    padding: 80px 0 0;
+  }
+`
 
 type LayoutProps = {
   title?: string
@@ -22,7 +33,7 @@ export default function LayoutGeneral({
     <>
       <CustomHead title={pageTitle} description={description}></CustomHead>
       <HeaderGeneral onCompleteReadingHandle={onCompleteReadingHandle} />
-      <main>{children}</main>
+      <Main>{children}</Main>
     </>
   )
 }
