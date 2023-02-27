a709462b58a70dcb878ebaf6bb5d2aee67b4af01
Tsuki Akiba <erase2004@gmail.com>
2023-02-27 14:25:47 +0800

    feat(readr): add FeatureCard component

---
+++ packages/readr/components/index/feature-card.tsx
@@ -0,0 +1,263 @@
+// 精選文章卡片 (might be legacy)
+
+import NextImage from 'next/image'
+import NextLink from 'next/link'
+import styled from 'styled-components'
+
+import { DEFAULT_POST_IMAGE_PATH } from '~/constants/constant'
+import useFallbackImage from '~/hooks/useFallbackImage'
+
+type StyledProps = {
+  $isFirst: boolean
+  $shouldShow: boolean
+}
+
+const Container = styled(NextLink)<Pick<StyledProps, '$isFirst'>>`
+  position: relative;
+  display: block;
+
+  picture {
+    position: relative;
+    display: block;
+    width: 100%;
+    overflow: hidden;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 50%;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      z-index: 50;
+      background: linear-gradient(
+        180deg,
+        rgba(0, 9, 40, 0) 0%,
+        rgba(0, 9, 40, 0.3) 100%
+      );
+    }
+    &::after {
+      content: '';
+      display: block;
+      padding-top: 52.5%;
+    }
+
+    img {
+      object-fit: cover;
+      object-position: center;
+      background-color: #d8d8d8;
+      z-index: 5;
+      transition: all 0.3s ease;
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+  }
+
+  ${({ theme, $isFirst }) =>
+    $isFirst &&
+    `
+      ${theme.breakpoint.xl} {
+        picture {
+          &::before {
+            top: 66.66%;
+          }
+        }
+      }
+    `}
+`
+
+const TextWrapper = styled.div<Pick<StyledProps, '$isFirst'>>`
+  position: absolute;
+  bottom: 8px;
+  left: 8px;
+  right: 8px;
+  text-align: left;
+  z-index: 70;
+  ${({ theme }) => theme.breakpoint.md} {
+    bottom: 16px;
+    left: 24px;
+    right: 24px;
+  }
+
+  ${({ theme, $isFirst }) =>
+    $isFirst &&
+    `
+      ${theme.breakpoint.xl} {
+        text-align: center;
+        bottom: 22px;
+        left: 200px;
+        right: 200px;
+      }
+    `}
+`
+
+const SharedDescWithEmojiStyles = `
+  line-height: 1.5;
+  letter-spacing: 0.03em;
+  color: #000928;
+  background-color: #ebf02c;
+  padding: 4px 8px;
+`
+
+const DescWithEmoji = styled.div<Pick<StyledProps, '$shouldShow'>>`
+  position: absolute;
+  display: block;
+  top: 0;
+  left: 0;
+  z-index: 70;
+  font-size: 16px;
+  ${SharedDescWithEmojiStyles}
+
+  ${({ theme }) => theme.breakpoint.md} {
+    font-size: 18px;
+  }
+
+  ${({ $shouldShow, theme }) =>
+    $shouldShow &&
+    `
+      ${theme.breakpoint.xl} {
+        display: none;
+      }
+    `}
+`
+
+const DescWithEmojiInTextWrapper = styled.div<Pick<StyledProps, '$shouldShow'>>`
+  display: none;
+
+  ${({ theme, $shouldShow }) =>
+    $shouldShow &&
+    `
+      ${theme.breakpoint.xl} {
+        display: inline-block;
+        font-size: 18px;
+        margin: 16px;
+        ${SharedDescWithEmojiStyles}
+      }
+    `}
+`
+
+const Emoji = styled.span`
+  font-weight: normal;
+  padding: 0 4px 0 0;
+`
+const Desc = styled.span`
+  font-weight: 700;
+`
+
+const Title = styled.div<Pick<StyledProps, '$isFirst'>>`
+  p {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 1.5;
+    letter-spacing: 0.03em;
+    color: #fff;
+    text-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+    word-wrap: break-word;
+    overflow: hidden;
+    ${({ theme }) => theme.breakpoint.md} {
+      font-size: 24px;
+    }
+    &:hover {
+      text-decoration: underline;
+      text-decoration-thickness: 1.5px;
+      text-underline-offset: 4px;
+    }
+
+    ${({ theme, $isFirst }) =>
+      $isFirst &&
+      `
+        ${theme.breakpoint.xl} {
+          font-size: 36px;
+          font-weight: 900;
+          &:hover {
+            text-decoration-thickness: 2px;
+            text-underline-offset: 8px;
+          }
+        }
+      `}
+  }
+`
+
+const Subtitle = styled.span`
+  display: none;
+  ${({ theme }) => theme.breakpoint.xl} {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    font-size: 18px;
+    line-height: 1.5;
+    color: #fff;
+    text-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+    word-wrap: break-word;
+    overflow: hidden;
+    margin: 8px 0 0;
+    padding: 0 80px;
+    &:hover {
+      text-decoration: underline;
+      text-decoration-thickness: 1px;
+      text-underline-offset: 4px;
+    }
+  }
+`
+
+export type FeatureCardProps = {
+  href: string
+  title: string
+  subtitle: string
+  image: string
+  description: string
+  isFirst: boolean
+}
+
+export default function FeatureCard({
+  href = '',
+  title = '',
+  subtitle = '',
+  image = DEFAULT_POST_IMAGE_PATH,
+  description = '',
+  isFirst = false,
+}: FeatureCardProps): JSX.Element {
+  const { imageSrc, onErrorHandle } = useFallbackImage(
+    image,
+    DEFAULT_POST_IMAGE_PATH
+  )
+
+  const [emoji, textWithoutEmoji] = description.split(' ')
+
+  return (
+    <Container href={href} target="_blank" $isFirst={isFirst}>
+      <picture>
+        <NextImage
+          src={imageSrc}
+          onError={onErrorHandle}
+          fill={true}
+          unoptimized={true}
+          alt={title}
+        />
+      </picture>
+      {title && (
+        <TextWrapper $isFirst={isFirst}>
+          {textWithoutEmoji && (
+            <DescWithEmojiInTextWrapper $shouldShow={isFirst}>
+              {emoji && <Emoji className="emoji">{emoji}</Emoji>}
+              <Desc className="desc">{textWithoutEmoji}</Desc>
+            </DescWithEmojiInTextWrapper>
+          )}
+          <Title $isFirst={isFirst}>
+            <p>{title}</p>
+          </Title>
+          {subtitle && isFirst && <Subtitle>{subtitle}</Subtitle>}
+        </TextWrapper>
+      )}
+      {textWithoutEmoji && (
+        <DescWithEmoji $shouldShow={isFirst}>
+          {emoji && <Emoji className="emoji">{emoji}</Emoji>}
+          <Desc className="desc">{textWithoutEmoji}</Desc>
+        </DescWithEmoji>
+      )}
+    </Container>
+  )
+}
