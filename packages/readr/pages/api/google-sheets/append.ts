c0e1cc7e875e2beb9bc29edb6154fa235b7fe872
Tsuki Akiba <erase2004@gmail.com>
2023-03-13 19:07:47 +0800

    refactor(readr): update google api related handle

--- packages/readr/pages/api/google-sheets/append.ts
+++ packages/readr/pages/api/google-sheets/append.ts
@@ -3,12 +3,7 @@ import { google } from 'googleapis'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { reportGoogleSheetApiError } from '~/utils/api'
-import { googleApiAuth } from '~/utils/google-api-auth'
-
-const sheets = google.sheets({
-  version: 'v4',
-  auth: googleApiAuth,
-})
+import { loadCredentails } from '~/utils/google-api-auth'
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,6 +12,17 @@ export default async function handler(
   const { body: sheetRequest } = req
 
   try {
+    const auth = await loadCredentails()
+
+    if (auth === null) {
+      throw new Error('failed to get Google API OAuth client')
+    }
+
+    const sheets = google.sheets({
+      version: 'v4',
+      auth,
+    })
+
     const { data, status = 200 } = await sheets.spreadsheets.values.append(
       sheetRequest
     )
--- packages/readr/utils/google-api-auth.ts
+++ packages/readr/utils/google-api-auth.ts
@@ -3,33 +3,37 @@
  * https://developers.google.com/sheets/api/quickstart/nodejs
  */
 
+import { readFile } from 'fs/promises'
 import { google } from 'googleapis'
+import { join } from 'path'
+import { cwd } from 'process'
 
-import CREDENTIALS from '~/constants/credentials.json'
-import TOKEN from '~/constants/token.json'
+const CREDENTIAL_PATH = join(cwd(), '/constants', 'credentials.json')
+const TOKEN_PATH = join(cwd(), '/constants', 'token.json')
 
-interface CredentialInfo {
-  installed: {
-    client_id?: string
-    client_secret?: string
-    redirect_uris?: string[]
+export async function loadCredentails() {
+  try {
+    let content = await readFile(CREDENTIAL_PATH)
+    const credential = JSON.parse(String(content))
+    const {
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uris: redirectUris = [],
+    } = credential.installed ?? {}
+    const oAuth2Client = new google.auth.OAuth2(
+      clientId,
+      clientSecret,
+      redirectUris[0]
+    )
+
+    content = await readFile(TOKEN_PATH)
+    const token = JSON.parse(String(content))
+    oAuth2Client.setCredentials(token)
+
+    return oAuth2Client
+  } catch (err) {
+    /* eslint-disable-next-line no-console */
+    console.log(err)
+    return null
   }
 }
-
-function authorize(credentials: CredentialInfo) {
-  const {
-    client_id: clientId,
-    client_secret: clientSecret,
-    redirect_uris: redirectUris = [],
-  } = credentials.installed ?? {}
-  const oAuth2Client = new google.auth.OAuth2(
-    clientId,
-    clientSecret,
-    redirectUris[0]
-  )
-  oAuth2Client.setCredentials(TOKEN)
-
-  return oAuth2Client
-}
-
-export const googleApiAuth = authorize(CREDENTIALS)
