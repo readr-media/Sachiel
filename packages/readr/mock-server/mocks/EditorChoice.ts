7afc1b43c3d50a62e2996f0edc4da1908d49ddf8
Tsuki Akiba <erase2004@gmail.com>
2023-03-23 14:28:46 +0800

    refactor(readr): update types in mock server

--- packages/readr/mock-server/mocks/EditorChoice.ts
+++ packages/readr/mock-server/mocks/EditorChoice.ts
@@ -1,3 +1,3 @@
 import type { GenericEditorChoice } from '../../types/common'
 
-export const EditorChoice: () => GenericEditorChoice = () => ({})
+export const EditorChoice: () => Partial<GenericEditorChoice> = () => ({})
--- packages/readr/mock-server/mocks/Post.ts
+++ packages/readr/mock-server/mocks/Post.ts
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker'
 import type { GenericPost } from '../../types/common'
 import { ValidPostStyle } from '../../types/common'
 
-export const Post: () => GenericPost = () => ({
+export const Post: () => Partial<GenericPost> = () => ({
   id: faker.datatype.uuid(),
   slug: faker.lorem.slug(),
   style: faker.helpers.arrayElement(Object.values(ValidPostStyle)),
