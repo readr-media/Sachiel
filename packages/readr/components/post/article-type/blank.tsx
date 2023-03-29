a8d79ba32ef3b254aa030459693de6dab1457951
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-03-29 11:02:38 +0800

    feat(post): add `blank` article-type

--- packages/readr/components/post/article-type/blank.tsx
+++ packages/readr/components/post/article-type/blank.tsx
@@ -1,3 +1,34 @@
-export default function Blank(): JSX.Element {
-  return <p>這是 style 為 Blank 的文章</p>
+import { Readr } from '@mirrormedia/lilith-draft-renderer'
+import styled from 'styled-components'
+
+import Footer from '~/components/layout/footer'
+import type { PostDetail } from '~/graphql/query/post'
+
+const BlankWrapper = styled.article`
+  background: transparent;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 650;
+`
+interface BlankProps {
+  postData: PostDetail
+}
+
+export default function Blank({ postData }: BlankProps): JSX.Element {
+  const { DraftRenderer } = Readr
+
+  //remove blocks[0] to avoid extra blank on top of liveblogs
+  const contentWithoutBlank = {
+    blocks: postData?.content.blocks.slice(1),
+    entityMap: postData?.content.entityMap,
+  }
+
+  return (
+    <BlankWrapper>
+      <DraftRenderer rawContentBlock={contentWithoutBlank} />
+      <Footer />
+    </BlankWrapper>
+  )
 }
