57956c17f39981be0838e595cbf0e2d8e177049a
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-03-31 13:18:29 +0800

    fix(post): modify `subscribe-button` structure

--- packages/readr/components/post/subscribe-button.tsx
+++ packages/readr/components/post/subscribe-button.tsx
@@ -1,7 +1,7 @@
 import { SubscribeButton } from '@readr-media/react-component'
 import styled from 'styled-components'
 
-const SubscribeWrapper = styled.section`
+const SubscribeWrapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
