7ba7e78148ed40d8784a0b4f733ee709e92cb941
bibibobobi <smilingcat33@gmail.com>
2023-04-07 18:37:47 +0800

    feat(readr/member): add member cards flip animation

--- packages/readr/components/about/members.tsx
+++ packages/readr/components/about/members.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 import ArrowLeft from '~/public/icons/arr-left.svg'
@@ -31,14 +32,53 @@ const Title = styled.h2`
   color: rgba(255, 255, 255, 0.87);
   margin-bottom: 20px;
   min-width: fit-content;
+
   ${({ theme }) => `
     ${theme.breakpoint.xl} {
       font-size: 36px;
     }
   `}
 `
+const CardsWrapper = styled.div`
+  /* background-color: aqua; */
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  ${({ theme }) => `
+    ${theme.breakpoint.md} {
+      margin-left: 5px;
+  `}
+
+  ${({ theme }) => `
+    ${theme.breakpoint.xl} {
+      margin-left: 120px;
+    }
+  `}
+`
 
 const Card = styled.div`
+  position: relative;
+  cursor: pointer;
+  margin: 0 0 20px 0;
+
+  ${({ theme }) => `
+    ${theme.breakpoint.sm} {
+      margin: 0 20px 20px 0;
+    }
+  `}
+
+  &.flipped {
+    .card-front {
+      transform: rotateY(180deg);
+    }
+    .card-back {
+      transform: rotateY(0deg);
+    }
+  }
+`
+
+const CardFront = styled.div`
   font-family: 'Noto Sans TC';
   background: #fff;
   width: 280px;
@@ -49,12 +89,16 @@ const Card = styled.div`
   justify-content: center;
   align-items: center;
   position: relative;
-
-  .arr-left {
-    position: absolute;
-    left: 8px;
-    top: 68px;
-  }
+  backface-visibility: hidden;
+  transition: transform 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  /* ${({ theme }) => `
+    ${theme.breakpoint.md} {
+      width: 230px;
+  `}
+  ${({ theme }) => `
+    ${theme.breakpoint.xl} {
+      width: 280px;
+  `} */
 
   .arr-right {
     position: absolute;
@@ -68,6 +112,21 @@ const InfoWrapper = styled.div`
   justify-content: center;
   align-items: center;
   padding: 12px 32px;
+
+  :hover {
+    cursor: pointer;
+    svg {
+      path {
+        fill: #000928;
+        fill-opacity: 1;
+        transition: 0.3s ease;
+      }
+    }
+    .arr-right {
+      transform: translateX(4px);
+      transition: transform 0.3s ease;
+    }
+  }
 `
 
 const Position = styled.div`
@@ -133,7 +192,63 @@ const Work = styled.div`
   &:hover {
     background-color: #04295e;
     color: #fff;
+    cursor: pointer;
   }
+`
+
+const CardBack = styled.div`
+  background: #ebf02c;
+  width: 280px;
+  min-height: 160px;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+  transition: transform 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  /* ${({ theme }) => `
+    ${theme.breakpoint.md} {
+      width: 230px;
+  `}
+  ${({ theme }) => `
+    ${theme.breakpoint.xl} {
+      width: 280px;
+  `} */
+  .arr-left {
+    position: absolute;
+    left: 8px;
+    top: 68px;
+  }
+  :hover {
+    svg {
+      path {
+        fill: #000928;
+        fill-opacity: 1;
+        transition: 0.3s ease;
+      }
+    }
+
+    .arr-left {
+      transform: translateX(-4px);
+      transition: transform 0.3s ease;
+    }
+  }
+  .number-back {
+    margin-bottom: 0;
+  }
+`
+
+const Caption = styled.div`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  padding: 10px 32px;
+  color: rgba(0, 9, 40, 0.66);
 `
 
 const members = {
@@ -168,48 +283,114 @@ const members = {
         number_desc_en: 'My working style',
         projects: [],
       },
+      {
+        id: '62',
+        isMember: true,
+        name: '章魚哥',
+        name_en: 'Patrick Star',
+        title: 'back-end engineer',
+        title_en: 'back-end engineer',
+        special_number: '2266',
+        number_desc: '我的工作風格',
+        number_desc_en: 'My working style',
+        projects: [
+          {
+            id: '1',
+            name: 'P#新聞實驗室',
+          },
+        ],
+      },
+      {
+        id: '63',
+        isMember: true,
+        name: '章魚燒',
+        name_en: 'Patrick Star',
+        title: 'back-end engineer',
+        title_en: 'back-end engineer',
+        special_number: '2266',
+        number_desc:
+          '如果很長很長，寫成一篇論文的話，如果真的有這麼多話要說的話，會怎麼樣呢？',
+        number_desc_en: 'My working style',
+        projects: [],
+      },
     ],
   },
 }
 
 export default function Members({ title }: { title: string }): JSX.Element {
+  const [flippedCards, setFlippedCards] = useState<string[]>([])
+
+  const handleClick = (
+    memberId: string,
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (flippedCards.includes(memberId)) {
+      setFlippedCards(flippedCards.filter((id) => id !== memberId))
+      console.log(`Card ${memberId} unflipped`)
+    } else {
+      setFlippedCards([...flippedCards, memberId])
+      console.log(`Card ${memberId} flipped`)
+    }
+    e.stopPropagation()
+  }
+
   return (
     <Container>
       <Title>{title}</Title>
-      {members.data.authors.map((member) => (
-        <Card key={member.id}>
-          <ArrowLeft className="arr-left" />
-          <ArrowRight className="arr-right" />
-          <InfoWrapper>
-            <Position>前端工程師</Position>
-            <Name data-name={member.name}>{member.name}</Name>
-            <Number>
-              {member.special_number.split('').map((digit, index) => {
-                return (
-                  <div className="digit" key={index}>
-                    {digit}
-                  </div>
-                )
-              })}
-            </Number>
-          </InfoWrapper>
-          {member.projects.length === 0 ? (
-            <Work
-              style={{
-                backgroundColor: '#E5E5E5',
-                color: 'grey',
-                pointerEvents: 'none',
-              }}
-            >
-              尚無相關作品
-            </Work>
-          ) : (
-            <Work>
-              <Link href={'/'}>相關作品</Link>
-            </Work>
-          )}
-        </Card>
-      ))}
+      <CardsWrapper>
+        {members.data.authors.map((member) => (
+          <Card
+            key={member.id}
+            onClick={(e) => handleClick(member.id, e)}
+            className={flippedCards.includes(member.id) ? 'flipped' : ''}
+          >
+            <CardFront className="card-front">
+              <InfoWrapper>
+                <ArrowRight className="arr-right" />
+                <Position>前端工程師</Position>
+                <Name data-name={member.name}>{member.name}</Name>
+                <Number>
+                  {member.special_number.split('').map((digit, index) => {
+                    return (
+                      <div className="digit" key={index}>
+                        {digit}
+                      </div>
+                    )
+                  })}
+                </Number>
+              </InfoWrapper>
+              {member.projects.length === 0 ? (
+                <Work
+                  style={{
+                    backgroundColor: '#E5E5E5',
+                    color: 'grey',
+                    pointerEvents: 'none',
+                  }}
+                >
+                  尚無相關作品
+                </Work>
+              ) : (
+                <Work onClick={(e) => e.stopPropagation()}>
+                  <Link href={'/'}>相關作品</Link>
+                </Work>
+              )}
+            </CardFront>
+            <CardBack className="card-back">
+              <ArrowLeft className="arr-left" />
+              <Number className="number-back">
+                {member.special_number.split('').map((digit, index) => {
+                  return (
+                    <div className="digit" key={index}>
+                      {digit}
+                    </div>
+                  )
+                })}
+              </Number>
+              <Caption>{member.number_desc}</Caption>
+            </CardBack>
+          </Card>
+        ))}
+      </CardsWrapper>
     </Container>
   )
 }
