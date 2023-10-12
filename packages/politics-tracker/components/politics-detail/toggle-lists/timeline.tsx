b7034559fe53a0a5d9f1982f5583b8d4e6ec4d6a
Xuan <as840922qq@gmail.com>
2023-10-12 22:25:09 +0800

    style(politics-detail): adjust `timeline` styles

--- packages/politics-tracker/components/politics-detail/toggle-lists/timeline.tsx
+++ packages/politics-tracker/components/politics-detail/toggle-lists/timeline.tsx
@@ -1,82 +1,97 @@
 import styled from 'styled-components'
 
 import DefaultText from '~/components/politics-detail/default-text'
+import type { TimeLine } from '~/types/politics-detail'
+import { getFormattedDate } from '~/utils/utils'
 
-const TimeContainer = styled.div`
-  padding-top: 20px;
-  margin-bottom: 25px;
-  font-weight: 500;
+const Wrapper = styled.div`
+  padding: 20px 0px 40px;
   font-size: 16px;
-  line-height: 1.3;
-  span {
-    color: #0f2d35;
-    display: block;
-    margin-bottom: 4px;
+  line-height: 1.5;
+`
+
+const TimeList = styled.li`
+  & + * {
+    margin-top: 15px;
   }
-  a {
+
+  ${({ theme }) => theme.breakpoint.md} {
+    display: flex;
+  }
+`
+
+const Time = styled.span`
+  color: ${({ theme }) => theme.textColor.black};
+  display: inline-block;
+  margin-bottom: 8px;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 6px;
+    height: 6px;
+    background-color: ${({ theme }) => theme.backgroundColor.landingYellow};
+    border-radius: 50%;
+    margin-right: 8px;
+    margin-bottom: 3px;
+  }
+
+  ${({ theme }) => theme.breakpoint.md} {
+    min-width: 100px;
+    margin: 0px 14px 0px 0px;
+  }
+`
+
+const ContentBlock = styled.div`
+  .content-text {
     color: ${({ theme }) => theme.textColor.brown};
     display: inline-block;
     word-break: break-all;
+    cursor: pointer;
+
     &:hover {
       text-decoration-line: underline;
-    }
-  }
-  li {
-    margin-bottom: 8px;
-    padding-left: 15px;
-    position: relative;
-    cursor: pointer;
-  }
-  li:before {
-    content: '';
-    display: inline-block;
-    background-color: #f7ba31;
-    border-radius: 50%;
-    width: 6px;
-    height: 6px;
-    text-align: center;
-    position: absolute;
-    left: 0px;
-    top: 10px;
-  }
-  div {
-    text-align: center;
-    color: rgba(15, 45, 53, 0.3);
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 1.8;
-  }
-  ${({ theme }) => theme.breakpoint.md} {
-    font-size: 18px;
-    li {
-      display: flex;
-    }
-    span {
-      min-width: 100px;
-      margin-right: 5px;
+      text-underline-offset: 3.5px;
+      text-decoration-thickness: 1.5px;
     }
   }
 `
 
-type TimeLineProps = {
-  infoList: any
+type TimelineProps = {
+  timelines: TimeLine[]
 }
-export default function Timeline({ infoList }: TimeLineProps): JSX.Element {
-  const info = infoList.map((value: any) => (
-    <li key={value.id}>
-      <span>{value.eventDate?.substr(0, 10)}</span>
-      <a href={value.link} target="_blank" rel="noreferrer">
-        {value.content}
-      </a>
-    </li>
-  ))
+export default function Timeline({
+  timelines = [],
+}: TimelineProps): JSX.Element {
+  const timeLists = timelines.map((item: TimeLine) => {
+    const { id, eventDate, link, content } = item
+
+    return (
+      <TimeList key={id}>
+        {eventDate && <Time>{getFormattedDate(eventDate)}</Time>}
+        <ContentBlock>
+          {content && (
+            <a
+              className="content-text"
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {content}
+            </a>
+          )}
+        </ContentBlock>
+      </TimeList>
+    )
+  })
+
   return (
-    <TimeContainer>
-      {infoList.length !== 0 ? (
-        <ul>{info}</ul>
+    <Wrapper>
+      {timelines.length > 0 ? (
+        <ul>{timeLists}</ul>
       ) : (
         <DefaultText title="相關進度" />
       )}
-    </TimeContainer>
+    </Wrapper>
   )
 }
