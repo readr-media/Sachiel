a1d83154e3795d8daba1bcae9f0ee1b2b680b1dc
Xuan <as840922qq@gmail.com>
2023-10-25 21:43:02 +0800

    style(landing/2022): adjust svg name

--- packages/politics-tracker/components/landing/election-2022/statistics-main.js
+++ packages/politics-tracker/components/landing/election-2022/statistics-main.js
@@ -183,7 +183,7 @@ export default function StatisticsBoard({ propsData }) {
             <ArrowIcon>
               <Image
                 alt="arrowRight"
-                src="/icons/landing/arrow_right.svg"
+                src="/icons/landing/arrow-right.svg"
                 width="48"
                 height="48"
               />
