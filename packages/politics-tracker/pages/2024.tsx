df757225afdc72b456ed13ad32d4da33f3fd5432
Xuan <as840922qq@gmail.com>
2023-10-25 21:38:38 +0800

    feat(landing/2024): get categories & factCheckJSON data

--- packages/politics-tracker/pages/2024.tsx
+++ packages/politics-tracker/pages/2024.tsx
@@ -1,8 +1,10 @@
+import axios from 'axios'
 import { print } from 'graphql'
 import type { GetServerSideProps } from 'next'
 import React from 'react'
 import styled from 'styled-components'
 
+import FactCheck from '~/components/landing/election-2024/fact-check-group'
 import HeroImage from '~/components/landing/election-2024/hero-image'
 import ButtonToLanding from '~/components/landing/shared/button-to-landing'
 import CollaborateGuide from '~/components/landing/shared/collaborate-guide'
@@ -11,9 +13,11 @@ import FileDownload from '~/components/landing/shared/file-download'
 import TeamIntro from '~/components/landing/shared/team-intro'
 import DefaultLayout from '~/components/layout/default'
 import { cmsApiUrl } from '~/constants/config'
+import { prefixUrlForLanding2024FactCheck } from '~/constants/config'
 import { fileDownload2024, teamIntro2024 } from '~/constants/landing'
+import GetPoliticCategories from '~/graphql/query/landing/get-exit-politic-categories.graphql'
 import GetFactCheckPartners from '~/graphql/query/landing/get-factcheck-partners.graphql'
-import type { FactCheckPartner } from '~/types/politics-detail'
+import type { FactCheckPartner, PoliticCategory } from '~/types/politics-detail'
 import { fireGqlRequest } from '~/utils/utils'
 
 const Main = styled.main`
@@ -27,20 +31,25 @@ const Main = styled.main`
 
 type Landing2024Props = {
   factCheckPartner: FactCheckPartner[]
+  categories: PoliticCategory[]
+  factCheckJSON: any
 }
 export default function Landing2024({
   factCheckPartner = [],
+  categories = [],
+  factCheckJSON = [],
 }: Landing2024Props): JSX.Element {
   return (
     <DefaultLayout>
       <Main>
         <HeroImage />
+        <FactCheck categories={categories} factCheckJSON={factCheckJSON} />
         <CollaborateGuide />
         <FileDownload links={fileDownload2024}>
           <ButtonToLanding
             title="2022 縣市長與議員政見"
             buttonText="2022 政見協作平台"
-            buttonLink="/"
+            buttonLink="/2022"
           />
         </FileDownload>
         <FactCheckPartners partners={factCheckPartner} />
@@ -59,6 +68,8 @@ export const getServerSideProps: GetServerSideProps<Landing2024Props> = async ({
   )
 
   let factCheckPartner: FactCheckPartner[] = []
+  let categories: PoliticCategory[] = []
+  let factCheckJSON: any = []
 
   try {
     {
@@ -71,7 +82,40 @@ export const getServerSideProps: GetServerSideProps<Landing2024Props> = async ({
         cmsApiUrl
       )
 
-      factCheckPartner = factcheckPartners
+      factCheckPartner = factcheckPartners || []
+    }
+
+    {
+      // get all politics categories
+      const {
+        data: { politicCategories },
+      } = await fireGqlRequest(print(GetPoliticCategories), {}, cmsApiUrl)
+
+      // sort categories by `politicsCount`
+      const orderedCategories = politicCategories.sort(
+        (a: PoliticCategory, b: PoliticCategory) =>
+          Number(b.politicsCount) - Number(a.politicsCount)
+      )
+
+      categories = orderedCategories || []
+    }
+
+    {
+      //get president:factCheck JSON   // FIXME: 錯誤處理：JSON 失效
+      const {
+        data: { personElections },
+      } = await axios.get(
+        `${prefixUrlForLanding2024FactCheck}/landing_factcheck_${categories[0].id}.json`
+      )
+
+      if (personElections.errors) {
+        throw new Error(
+          'JSON errors: Landing2024 President FactCheck Error' +
+            JSON.stringify(personElections.errors)
+        )
+      }
+
+      factCheckJSON = personElections || []
     }
   } catch (err) {
     console.error(err)
@@ -83,6 +127,8 @@ export const getServerSideProps: GetServerSideProps<Landing2024Props> = async ({
   return {
     props: {
       factCheckPartner,
+      categories,
+      factCheckJSON,
     },
   }
 }
