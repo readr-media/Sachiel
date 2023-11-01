393f31c182ba7f2132695c9e5870ce8595ff20a7
Xuan <as840922qq@gmail.com>
2023-11-01 20:55:30 +0800

    feat(pt): add GTM whole-site setting

--- packages/politics-tracker/pages/_document.tsx
+++ packages/politics-tracker/pages/_document.tsx
@@ -1,7 +1,10 @@
 import type { DocumentContext } from 'next/document'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import React from 'react'
 import { ServerStyleSheet } from 'styled-components'
+
+import { gtmId } from '~/constants/environment-variables'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -69,10 +72,31 @@ export default class MyDocument extends Document {
             key="msapplication-TileColor"
           />
           <meta name="theme-color" content="#04295e" key="theme-color" />
+
+          <Script
+            id="google-tag-manager"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');`,
+            }}
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
         </body>
       </Html>
     )
