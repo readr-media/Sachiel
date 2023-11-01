0af999657161da2853e81a2e7bdeb55693d2d104
Xuan <as840922qq@gmail.com>
2023-11-01 22:32:06 +0800

    refactor(pt): add props `image` & adjust meta structures

--- packages/politics-tracker/components/custom-head.tsx
+++ packages/politics-tracker/components/custom-head.tsx
@@ -25,59 +25,7 @@ export type OGProperties = {
 export type HeadProps = {
   title?: string
   description?: string
-}
-
-const OpenGraph = ({ properties }: { properties: OGProperties }) => {
-  const { locale, url, site_name, title, type, description, image, card } =
-    properties
-
-  return (
-    <>
-      <meta property="og:locale" content={locale || 'zh_TW'} key="og:locale" />
-      <meta property="og:title" content={title} key="og:title" />
-      <meta property="og:type" content={type} key="og:type" />
-      <meta
-        property="og:description"
-        content={description || ''}
-        key="og:description"
-      />
-      <meta property="og:site_name" content={site_name} key="og:site_name" />
-      {image && (
-        <>
-          <meta property="og:image" content={image.url} key="og:image" />
-          <meta
-            property="og:image:secure_url"
-            content={image.url.replace('http://', 'https://')}
-            key="og:image:secure_url"
-          />
-          <meta
-            property="og:image:width"
-            content={image.width}
-            key="og:image:width"
-          />
-          <meta
-            property="og:image:height"
-            content={image.height}
-            key="og:image:height"
-          />
-          <meta
-            property="og:image:type"
-            content={image.type}
-            key="og:image:type"
-          />
-          <meta name="twitter:image" content={image.url} key="twitter:image" />
-        </>
-      )}
-      <meta name="twitter:card" content={card} key="twitter:card" />
-      <meta name="twitter:url" content={url} key="twitter:url" />
-      <meta name="twitter:title" content={title} key="twitter:title" />
-      <meta
-        name="twitter:description"
-        content={description || ''}
-        key="twitter:description"
-      />
-    </>
-  )
+  image?: string
 }
 
 export default function CustomHead(props: HeadProps): JSX.Element {
@@ -93,7 +41,7 @@ export default function CustomHead(props: HeadProps): JSX.Element {
       width: '1200',
       height: '630',
       type: 'images/jpeg',
-      url: `${siteUrl}/og.jpg`,
+      url: `${siteUrl}/${props.image ?? 'og.jpg'}`,
     },
     card: 'summary_large_image',
   }
@@ -106,7 +54,83 @@ export default function CustomHead(props: HeadProps): JSX.Element {
         content={siteInformation.description}
         key="description"
       />
-      <OpenGraph properties={siteInformation} />
+
+      <meta
+        property="og:locale"
+        content={siteInformation.locale || 'zh_TW'}
+        key="og:locale"
+      />
+      <meta
+        property="og:title"
+        content={siteInformation.title}
+        key="og:title"
+      />
+      <meta property="og:type" content={siteInformation.type} key="og:type" />
+      <meta
+        property="og:description"
+        content={siteInformation.description || ''}
+        key="og:description"
+      />
+      <meta
+        property="og:site_name"
+        content={siteInformation.site_name}
+        key="og:site_name"
+      />
+      {siteInformation.image && (
+        <>
+          <meta
+            property="og:image"
+            content={siteInformation.image.url}
+            key="og:image"
+          />
+          <meta
+            property="og:image:secure_url"
+            content={siteInformation.image.url.replace('http://', 'https://')}
+            key="og:image:secure_url"
+          />
+          <meta
+            property="og:image:width"
+            content={siteInformation.image.width}
+            key="og:image:width"
+          />
+          <meta
+            property="og:image:height"
+            content={siteInformation.image.height}
+            key="og:image:height"
+          />
+          <meta
+            property="og:image:type"
+            content={siteInformation.image.type}
+            key="og:image:type"
+          />
+          <meta
+            name="twitter:image"
+            content={siteInformation.image.url}
+            key="twitter:image"
+          />
+        </>
+      )}
+      <meta
+        name="twitter:card"
+        content={siteInformation.card}
+        key="twitter:card"
+      />
+      <meta
+        name="twitter:url"
+        content={siteInformation.url}
+        key="twitter:url"
+      />
+      <meta
+        name="twitter:title"
+        content={siteInformation.title}
+        key="twitter:title"
+      />
+      <meta
+        name="twitter:description"
+        content={siteInformation.description || ''}
+        key="twitter:description"
+      />
+
       <meta name="application-name" content={siteInformation.title} />
     </Head>
   )
