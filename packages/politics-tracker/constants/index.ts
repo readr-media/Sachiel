16b3a53a9a30ebfc6078bb2f645d83fb92b663b1
Tsuki Akiba <erase2004@gmail.com>
2023-11-08 13:27:28 +0800

    refactor(politics-tracker): update z-index values

--- packages/politics-tracker/constants/index.ts
+++ packages/politics-tracker/constants/index.ts
@@ -1,8 +1,9 @@
 const Z_INDEX = {
   top: 10000,
-  coverHeader: 2000,
+  overHeader: 1100,
   header: 1000,
-  coverContent: 100,
+  belowHeader: 900,
+  overContent: 200,
 }
 
 const FB_PAGES_ID = '563994370665617'
