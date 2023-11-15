1ed63720c6d161821e00f66b34ec8e6f2252da20
bibibobobi <smilingcat33@gmail.com>
2023-11-15 17:02:49 +0800

    feat(pt): add anchor id based on the election year

--- packages/politics-tracker/components/politics/section-list.tsx
+++ packages/politics-tracker/components/politics/section-list.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect, useRef, useState } from 'react'
 
 import type { LegislatorAtLarge, PersonElection } from '~/types/politics'
 
@@ -16,7 +17,36 @@ type SectionListProps = PersonElection & {
 }
 
 export default function SectionList(props: SectionListProps): JSX.Element {
-  const [isActive, setIsActive] = useState<boolean>(props.order === 0)
+  const [isActive, setIsActive] = useState<boolean>(false)
+  const router = useRouter()
+  const anchorRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const yearFromAnchor = router.asPath.split('#')[1] // Get the year value from the anchor in the URL
+
+    if (yearFromAnchor) {
+      const extractedYear = parseInt(yearFromAnchor)
+      if (Array.isArray(props.year)) {
+        setIsActive(props.year.includes(extractedYear)) // Set isActive based on array inclusion
+      } else {
+        setIsActive(props.year === extractedYear) // Set isActive for single number comparison
+      }
+    } else {
+      // If there's no year in the anchor or no anchor present, use default props.order === 0 to set isActive
+      setIsActive(props.order === 0)
+    }
+  }, [router.asPath, props.year, props.order])
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isActive && anchorRef.current) {
+        const scroll = () => {
+          anchorRef?.current?.scrollIntoView({ behavior: 'smooth' })
+        }
+        requestAnimationFrame(scroll)
+      }
+    })
+  }, [isActive, router.asPath])
 
   return (
     <PersonElectionContext.Provider value={props}>
@@ -26,6 +56,7 @@ export default function SectionList(props: SectionListProps): JSX.Element {
           content={props.name}
           isActive={isActive}
           setActive={() => setIsActive(!isActive)}
+          id={String(props.year)}
         />
         <SectionBody
           show={isActive}
