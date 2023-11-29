c04f118b81a0a0fce3d8a67396cc667e59e1d352
Tsuki Akiba <erase2004@gmail.com>
2023-11-30 00:01:20 +0800

    refactor(politics-tracker): update comments on types

--- packages/politics-tracker/types/common.ts
+++ packages/politics-tracker/types/common.ts
@@ -5,8 +5,11 @@ import { POLITIC_PROGRESS } from '~/constants/common'
 
 export type LinkHref = LinkProps['href']
 
+/** generic type for GraphQL response */
 export type GenericGQLData<T, U extends string> = {
+  /** 資料 */
   data?: Record<U, T>
+  /** 錯誤 */
   errors: any[]
 }
 
@@ -22,318 +25,467 @@ export type StatusOptionsA = 'active' | 'deactive'
 
 export type StatusOptionsB = 'verified' | 'notverified'
 
+/** 來源 */
 export type Source = {
   id: string
+  /** 內容 */
   value: string
+  /** 錯誤 */
   error: string
 }
 
-/* eslint-disable-next-line no-unused-vars */
-export type OrderFunction<T> = (datas: T[]) => T[]
+/** generic type for sort function */
+export type OrderFunction<T> = (
+  /* eslint-disable no-unused-vars */ datas: T[]
+) => T[]
 
-// This utility is for overwriting type without extending it
 // prettier-ignore
+/** This utility is for overwriting type without extending it */
 export type Override<T, U extends Partial<Record<keyof T, unknown>>> = Omit<T, keyof U> & U
 
 export type ExtendedOption = Option & { sortOrder: number }
 export type FormConfig = Record<'formId' | 'fieldId', string>
 export type FeedbackFormConfig = Record<'emoji' | 'text', FormConfig>
 
+/** Make members of type to be nonullable */
 export type NotNullableAllMemberOfType<T> = {
   [P in keyof T]: NonNullable<T[P]>
 }
 
 //--- Following types should be related to data sources ---//
 
+/** 各尺寸圖片 */
 export type RawResizedImages = {
+  /** 原圖網址 */
   original: string
 }
 
-// 圖片
+/** 圖片 */
 export type RawPhoto = {
   id: string
-  name: string // 標題
+  /** 標題 */
+  name: string
   resized: RawResizedImages
   urlOriginal: string
 }
 
-// 標籤
+/** 標籤 */
 export type RawTag = {
   id: string
-  name: string // 標籤名稱
-  brief: string // 前言
-  ogTitle: string // FB分享標題
-  ogDescription: string // FB分享說明
-  isFeatured: boolean // 置頂
+  /** 標籤名稱 */
+  name: string
+  /** 前言 */
+  brief: string
+  /** FB分享標題 */
+  ogTitle: string
+  /** FB分享說明 */
+  ogDescription: string
+  /** 置頂 */
+  isFeatured: boolean
 }
 
-// 選區
+/** 選區 */
 export type RawElectionArea = {
   id: string
-  name: string // 選舉區名稱
-  city: string // 城市
+  /** 選舉區名稱 */
+  name: string
+  /** 城市 */
+  city: string
   description: string
   status: StatusOptionsA
 }
 
-// 選舉
+/** 選舉 */
 export type RawElection = {
   id: string
   name: string
   description: string
-  election_year_year: number // 選舉年
-  election_year_month: number // 選舉月
-  election_year_day: number // 選舉日
-  level: string // 選舉層級
-  type: string // 選舉目的（種類）
-  register_date: string // 登記日期
-  location: string // 位置
+  /** 選舉年 */
+  election_year_year: number
+  /** 選舉月 */
+  election_year_month: number
+  /** 選舉日 */
+  election_year_day: number
+  /** 選舉層級 */
+  level: string
+  /** 選舉目的（種類） */
+  type: string
+  /** 登記日期 */
+  register_date: string
+  /** 位置 */
+  location: string
   electionArea: RawElectionArea[]
   status: StatusOptionsA
-  hidePoliticDetail: string // 隱藏政見細節
-  addComments: boolean // 開放留言（心情）
+  /** 隱藏政見細節 */
+  hidePoliticDetail: string
+  /** 開放留言（心情） */
+  addComments: boolean
   createdAt: string
   updatedAt: string
 }
 
-// 人物
+/** 人物 */
 export type RawPerson = {
   id: string
-  name: string // 姓名
-  alternative: string | null // 別名
-  other_names: string | null // 舊名
+  /** 姓名 */
+  name: string
+  /** 別名 */
+  alternative: string | null
+  /** 舊名 */
+  other_names: string | null
   email: string | null
   gender: string
-  birth_date_year: number | null // 出生年
-  birth_date_month: number | null // 出生月
-  birth_date_day: number | null // 出生日
-  death_date_year: number | null // 死亡年
-  death_date_month: number | null // 死亡月
-  death_date_day: number | null // 死亡日
-  image: string | null // 大頭照
-  summary: string | null // 一句話描寫這個人
-  biography: string | null // 詳細生平
+  /** 出生年 */
+  birth_date_year: number | null
+  /** 出生月 */
+  birth_date_month: number | null
+  /** 出生日 */
+  birth_date_day: number | null
+  /** 死亡年 */
+  death_date_year: number | null
+  /** 死亡月 */
+  death_date_month: number | null
+  /** 死亡日 */
+  death_date_day: number | null
+  /** 大頭照 */
+  image: string | null
+  /** 一句話描寫這個人 */
+  summary: string | null
+  /** 詳細生平 */
+  biography: string | null
   national_identity: string | null
   contact_details: string | null
   links: string | null
   source: string | null
   status: StatusOptionsB
-  thread_parent: RawPerson | null // 補充資料
-  tags: RawTag[] // 標籤
+  /** 補充資料 */
+  thread_parent: RawPerson | null
+  /** 標籤 */
+  tags: RawTag[]
   createdAt: string
   updatedAt: string
 }
 
-// 組織
+/** 組織 */
 export type RawOrganization = {
   id: string
-  name: string // 組織名稱
-  alternative: string | null // 組織別名
-  other_names: string | null // 組織舊名
-  identifiers: string | null // 統一編號
-  classification: string // 組織類型
-  abstract: string | null // 一句話描述該組織
-  description: string | null // 組織詳細介紹
-  founding_date_year: number | null // 組織成立年
-  founding_date_month: number | null // 組織成立月
-  founding_date_day: number | null // 組織成立日
-  dissolution_date_year: number | null // 組織解散年
-  dissolution_date_month: number | null // 組織解散月
-  dissolution_date_day: number | null // 組織解散日
-  image: string | null // 圖像
+  /** 組織名稱 */
+  name: string
+  /** 組織別名 */
+  alternative: string | null
+  /** 組織舊名 */
+  other_names: string | null
+  /** 統一編號 */
+  identifiers: string | null
+  /** 組織類型 */
+  classification: string
+  /** 一句話描述該組織 */
+  abstract: string | null
+  /** 組織詳細介紹 */
+  description: string | null
+  /** 組織成立年 */
+  founding_date_year: number | null
+  /** 組織成立月 */
+  founding_date_month: number | null
+  /** 組織成立日 */
+  founding_date_day: number | null
+  /** 組織解散年 */
+  dissolution_date_year: number | null
+  /** 組織解散月 */
+  dissolution_date_month: number | null
+  /** 組織解散日 */
+  dissolution_date_day: number | null
+  /** 圖像 */
+  image: string | null
   contact_details: string | null
   links: string | null
-  address: string | null // 組織稅籍登記地址
+  /** 組織稅籍登記地址 */
+  address: string | null
   source: string | null
   status: StatusOptionsB
-  tags: RawTag[] // 標籤
+  /** 標籤 */
+  tags: RawTag[]
   reviewed: boolean
   createdAt: string
   updatedAt: string
 }
 
-// 人物 - 選舉關係
+/** 人物 - 選舉關係 */
 export type RawPersonElection = {
   id: string
   name: string
-  person_id: RawPerson | null // 人物
-  election: RawElection | null // 選舉
-  mainCandidate: RawPersonElection | null // 搭配主要候選人
-  party: RawOrganization | null // 推薦政黨
-  legislatoratlarge_number: string // 不分區立委排序
-  number: string // 號次
-  electoral_district: RawElectionArea | null // 選區
-  votes_obtained_number: string // 得票數
-  votes_obtained_percentage: string // 得票率
+  /** 人物 */
+  person_id: RawPerson | null
+  /** 選舉 */
+  election: RawElection | null
+  /** 搭配主要候選人 */
+  mainCandidate: RawPersonElection | null
+  /** 推薦政黨 */
+  party: RawOrganization | null
+  /** 不分區立委排序 */
+  legislatoratlarge_number: string
+  /** 號次 */
+  number: string
+  /** 選區 */
+  electoral_district: RawElectionArea | null
+  /** 得票數 */
+  votes_obtained_number: string
+  /** 得票率 */
+  votes_obtained_percentage: string
   elected: boolean
-  incumbent: boolean // 是否現任
-  source: string // 資料來源
-  politicSource: string // 政見資料來源
+  /** 是否現任 */
+  incumbent: boolean
+  /** 資料來源 */
+  source: string
+  /** 政見資料來源 */
+  politicSource: string
 }
 
-// 組織 - 選舉關係
+/** 組織 - 選舉關係 */
 export type RawOrganizationElection = {
   id: string
-  organization_id: RawOrganization | null // 組織名稱
-  elections: RawElection | null // 選舉
-  number: string // 號次
-  votes_obtained_number: string // 得票數
+  /** 組織名稱 */
+  organization_id: RawOrganization | null
+  /** 選舉 */
+  elections: RawElection | null
+  /** 號次 */
+  number: string
+  /** 得票數 */
+  votes_obtained_number: string
   source: string
-  election_year_year: number // 選舉年
-  election_year_month: number // 選舉月
-  election_year_day: number // 選舉日
-  seats: string // 分配席次
-  politics: RawPolitic[] // 政見
+  /** 選舉年 */
+  election_year_year: number
+  /** 選舉月 */
+  election_year_month: number
+  /** 選舉日 */
+  election_year_day: number
+  /** 分配席次 */
+  seats: string
+  /** 政見 */
+  politics: RawPolitic[]
 }
 
-// 政見
+/** 政見 */
 export type RawPolitic = {
   id: string
-  person: RawPersonElection | null // 候選人 - 選舉
-  organization: RawOrganizationElection | null // 政黨 - 選舉
-  desc: string // 政見
-  content: string // 政策補充說明
+  /** 候選人 - 選舉關係 */
+  person: RawPersonElection | null
+  /** 政黨 - 選舉關係 */
+  organization: RawOrganizationElection | null
+  /** 政見 */
+  desc: string
+  /** 政策補充說明 */
+  content: string
   source: string
-  contributer: string // 資料提供
-  current_progress: `${POLITIC_PROGRESS}` // 政見進度
+  /** 資料提供 */
+  contributer: string
+  /** 政見進度 */
+  current_progress: `${POLITIC_PROGRESS}`
   status: StatusOptionsB
   checked: boolean
   reviewed: Boolean
-  thread_parent: RawPolitic | null // 修改備註
-  politicCategory: RawPoliticCategory | null // 類別
-  changeLog: string // 修改備註
-  positionChange: RawPoliticPositionChange[] // 立場變化
-  factCheck: RawFactCheck[] // 事實查核
-  repeat: RawPoliticRepeat[] // 重複政見
-  controversies: RawPoliticControversy[] // 爭議內容
-  expertPoint: RawExpertPoint[] // 專家觀點
-  response: RawPoliticResponse[] // 政見回應
-  timeline: RawPoliticTimeline[] // 時間軸
+  /** 修改備註 */
+  thread_parent: RawPolitic | null
+  /** 類別 */
+  politicCategory: RawPoliticCategory | null
+  /** 修改備註 */
+  changeLog: string
+  /** 立場變化 */
+  positionChange: RawPoliticPositionChange[]
+  /** 事實查核 */
+  factCheck: RawFactCheck[]
+  /** 重複政見 */
+  repeat: RawPoliticRepeat[]
+  /** 爭議內容 */
+  controversies: RawPoliticControversy[]
+  /** 專家觀點 */
+  expertPoint: RawExpertPoint[]
+  /** 政見回應 */
+  response: RawPoliticResponse[]
+  /** 時間軸 */
+  timeline: RawPoliticTimeline[]
   createdAt: string
   updatedAt: string
 }
 
-// 政見類別
+/** 政見類別 */
 export type RawPoliticCategory = {
   id: string
   politics: RawPolitic[]
-  name: string // 標籤名稱
-  brief: string // 前言
-  displayColor: string // 頁面色碼
-  ogTitle: string // FB分享標題
-  ogDescription: string // FB分享說明
-  isFeatured: boolean // 置頂
+  /** 標籤名稱 */
+  name: string
+  /** 前言 */
+  brief: string
+  /** 頁面色碼 */
+  displayColor: string
+  /** FB 分享標題 */
+  ogTitle: string
+  /** FB 分享說明 */
+  ogDescription: string
+  /** 置頂 */
+  isFeatured: boolean
 }
 
-// 政見立場變化
+/** 政見立場變化 */
 export type RawPoliticPositionChange = {
   id: string
   politic: RawPolitic[]
   editingPolitic: RawPolitic[]
-  positionChangeSummary: string // 立場變化（摘要）
-  isChanged: string // 立場改變
-  content: string // 查核內容
+  /** 立場變化（摘要） */
+  positionChangeSummary: string
+  /** 立場改變 */
+  isChanged: string
+  /** 查核內容 */
+  content: string
   checkDate: string
-  link: string // 相關連結
-  factcheckPartner: RawFactCheckPartner | null // 查核單位
+  /** 相關連結 */
+  link: string
+  /** 查核單位 */
+  factcheckPartner: RawFactCheckPartner | null
 }
 
-// 政見事實查核
+/** 政見事實查核 */
 export type RawFactCheck = {
   id: string
   politic: RawPolitic[]
   editingPolitic: RawPolitic[]
-  factCheckSummary: string // 事實查核（摘要）
-  checkResultType: string // 查核結果
-  checkResultOther: string // 查核結果（其他）
-  content: string // 查核內容
-  link: string // 相關連結
-  factcheckPartner: RawFactCheckPartner | null // 查核單位
+  /** 事實查核（摘要） */
+  factCheckSummary: string
+  /** 查核結果 */
+  checkResultType: string
+  /** 查核結果（其他） */
+  checkResultOther: string
+  /** 查核內容 */
+  content: string
+  /** 相關連結 */
+  link: string
+  /** 查核單位 */
+  factcheckPartner: RawFactCheckPartner | null
 }
 
-// 重複政見
+/** 重複政見 */
 export type RawPoliticRepeat = {
   id: string
   politic: RawPolitic[]
   editingPolitic: RawPolitic[]
-  repeatSummary: string // 重複政見（摘要）
-  content: string // 重複內容
-  link: string // 相關連結
-  contributer: string // 資料提供
-  factcheckPartner: RawFactCheckPartner | null // 查核單位
+  /** 重複政見（摘要） */
+  repeatSummary: string
+  /** 重複內容 */
+  content: string
+  /** 相關連結 */
+  link: string
+  /** 資料提供 */
+  contributer: string
+  /** 查核單位 */
+  factcheckPartner: RawFactCheckPartner | null
 }
 
-// 政見爭議內容
+/** 政見爭議內容 */
 export type RawPoliticControversy = {
   id: string
   politic: RawPolitic[]
   editingPolitic: RawPolitic[]
-  content: string // 爭議內容
-  link: string // 相關連結
-  factcheckPartner: RawFactCheckPartner | null // 查核單位
+  /** 爭議內容 */
+  content: string
+  /** 相關連結 */
+  link: string
+  /** 查核單位 */
+  factcheckPartner: RawFactCheckPartner | null
 }
 
-// 查核單位
+/** 查核單位 */
 export type RawFactCheckPartner = {
   id: string
-  name: string // 組織名稱
-  type: string // 合作形式
-  webUrl: string // 網站網址
-  logo: RawPhoto | null // 大 Logo 網址（for landing page）
-  slogo: RawPhoto | null // 小 Logo 網址（for politic page）
-  year: string // 年份
+  /** 組織名稱 */
+  name: string
+  /** 合作形式 */
+  type: string
+  /** 網站網址 */
+  webUrl: string
+  /** 大 Logo 網址（for landing page） */
+  logo: RawPhoto | null
+  /** 小 Logo 網址（for politic page） */
+  slogo: RawPhoto | null
+  /** 年份 */
+  year: string
   positionChange: RawPoliticPositionChange[]
   factCheck: RawFactCheck[]
   repeat: RawPoliticRepeat[]
 }
 
-// 專家觀點
+/** 專家觀點 */
 export type RawExpertPoint = {
   id: string
   politic: RawPolitic[]
   editingPolitic: RawPolitic
-  expertPointSummary: string // 專家觀點（摘要）
-  expert: string // 專家姓名
-  avatar: string // 頭像連結
-  content: string // 意見內容
-  link: string // 連結
-  title: string // 職稱
-  contributer: string // 資料提供
+  /** 專家觀點（摘要） */
+  expertPointSummary: string
+  /** 專家姓名 */
+  expert: string
+  /** 頭像連結 */
+  avatar: string
+  /** 意見內容 */
+  content: string
+  /** 連結 */
+  link: string
+  /** 職稱 */
+  title: string
+  /** 資料提供 */
+  contributer: string
 }
 
-// 候選人回應
+/** 候選人回應 */
 export type RawPoliticResponse = {
   id: string
-  politic: RawPolitic[] // 政見
+  /** 政見 */
+  politic: RawPolitic[]
   editingPolitic: RawPolitic[]
-  responseName: string // 回應者姓名
-  responsePic: string // 回應者頭像
-  responseTitle: string // 回應者身分
-  content: string // 回應內容
-  link: string // 相關連結
+  /** 回應者姓名 */
+  responseName: string
+  /** 回應者頭像 */
+  responsePic: string
+  /** 回應者身分 */
+  responseTitle: string
+  /** 回應內容 */
+  content: string
+  /** 相關連結 */
+  link: string
 }
 
-// 政見進度
+/** 政見進度 */
 export type RawPoliticTimeline = {
   id: string
   politic: RawPolitic[]
   editingPolitic: RawPolitic[]
-  eventDate: string // 日期
-  sortOrder: number // 排序
-  content: string // 文字
-  link: string // 連結
-  contributer: string // 資料提供
+  /** 日期 */
+  eventDate: string
+  /** 排序 */
+  sortOrder: number
+  /** 文字 */
+  content: string
+  /** 連結 */
+  link: string
+  /** 資料提供 */
+  contributer: string
 }
 
-// 人物 - 組織關係
+/** 人物 - 組織關係 */
 export type RawPersonOrgnization = {
   id: string
-  person_id: RawPerson | null // 姓名
-  start_date_day: number | null // 開始年
-  start_date_month: number | null // 開始月
-  start_date_year: number | null // 開始日
-  end_date_day: number | null // 結束年
-  end_date_month: number | null // 結束月
-  end_date_year: number | null // 結束日
-  organization_id: RawOrganization | null // 組織名稱
+  /** 姓名 */
+  person_id: RawPerson | null
+  /** 開始年 */
+  start_date_day: number | null
+  /** 開始月 */
+  start_date_month: number | null
+  /** 開始日 */
+  start_date_year: number | null
+  /** 結束年 */
+  end_date_day: number | null
+  /** 結束月 */
+  end_date_month: number | null
+  /** 結束日 */
+  end_date_year: number | null
+  /** 組織名稱 */
+  organization_id: RawOrganization | null
 }
