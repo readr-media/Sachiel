d8c7e76e7ce0f5eccdd6b23490c441da30d38391
Xuan <as840922qq@gmail.com>
2024-01-01 23:15:30 +0800

    feat(pt/detail): add editing-detail

--- packages/politics-tracker/components/politics-detail/toggle-lists/detail.tsx
+++ packages/politics-tracker/components/politics-detail/toggle-lists/detail.tsx
@@ -1,7 +1,17 @@
+import { print } from 'graphql'
 import styled from 'styled-components'
 
+import type { DraftPolitic } from '~/components/politics/politic-form'
+import PoliticForm from '~/components/politics/politic-form'
 import Sources from '~/components/politics-detail/sources'
+import { useToast } from '~/components/toast/use-toast'
 import { SOURCE_DELIMITER } from '~/constants/politics'
+import AddEditingPoliticToThread from '~/graphql/mutation/politics/add-editing-politic-to-thread.graphql'
+import type { GenericGQLData } from '~/types/common'
+import type { CreatedEditingPoliticData } from '~/types/politics'
+import type { PoliticDetail } from '~/types/politics-detail'
+import { isDraftPoliticForModification } from '~/utils/politic'
+import { fireGqlRequest } from '~/utils/utils'
 
 const DetailContainer = styled.div`
   padding: 20px 0px;
@@ -22,6 +32,7 @@ const DetailList = styled.li`
     color: #0f2d35;
     margin-bottom: 12px;
   }
+
   > span {
     display: inline-block;
     font-style: normal;
@@ -32,6 +43,7 @@ const DetailList = styled.li`
     margin-bottom: 10px;
     padding-left: 15px;
   }
+
   &:before {
     content: '';
     display: inline-block;
@@ -56,33 +68,128 @@ const DetailList = styled.li`
 `
 
 type DetailsProps = {
-  desc: string
-  additional: string
-  source: string
+  politic: PoliticDetail
+  editMode: boolean
+  setEditMode: React.Dispatch<React.SetStateAction<boolean>>
 }
 export default function Details({
-  desc = '',
-  additional = '',
-  source = '',
+  politic,
+  setEditMode = () => {},
+  editMode = false,
 }: DetailsProps): JSX.Element {
+  const { desc, content, source, person, organization } = politic
+
+  const toast = useToast()
+
+  async function appendPoliticToThread(data: DraftPolitic): Promise<boolean> {
+    if (!person || !organization) throw new Error('electionData is null')
+    if (!isDraftPoliticForModification(data)) throw Error('`id` is not in data')
+
+    const cmsApiUrl = `${window.location.origin}/api/data`
+    const isPartyPage = false
+
+    try {
+      let variables: any
+
+      if (isPartyPage) {
+        variables = {
+          data: {
+            thread_parent: {
+              connect: {
+                id: data.id,
+              },
+            },
+            organization: {
+              connect: {
+                id: organization?.id,
+              },
+            },
+            desc: data.desc,
+            source: data.source,
+            content: data.content,
+          },
+        }
+      } else {
+        variables = {
+          data: {
+            thread_parent: {
+              connect: {
+                id: data.id,
+              },
+            },
+            person: {
+              connect: {
+                id: person?.id,
+              },
+            },
+            desc: data.desc,
+            source: data.source,
+            content: data.content,
+          },
+        }
+      }
+
+      // result is not used currently
+      // eslint-disable-next-line
+      const result: GenericGQLData<
+        CreatedEditingPoliticData,
+        'createEditingPolitic'
+      > = await fireGqlRequest(
+        print(AddEditingPoliticToThread),
+        variables,
+        cmsApiUrl
+      )
+
+      toast.open({
+        status: 'success',
+        title: '送出成功',
+        desc: '通過志工審核後，您修改的政見就會出現在這裡',
+      })
+
+      setEditMode(false)
+
+      return true
+    } catch (err) {
+      console.error(err)
+
+      toast.open({
+        status: 'fail',
+        title: '出了點問題...',
+        desc: '送出失敗，請重試一次',
+      })
+
+      return false
+    }
+  }
+
   return (
     <DetailContainer>
-      <DetailList>
-        <span>政見</span>
-        {desc.split(SOURCE_DELIMITER).map((item) => {
-          return <p key={item}>{item}</p>
-        })}
-      </DetailList>
+      {editMode ? (
+        <PoliticForm
+          politic={politic}
+          closeForm={() => setEditMode(false)}
+          submitForm={appendPoliticToThread}
+        />
+      ) : (
+        <>
+          <DetailList>
+            <span>政見</span>
+            {desc.split(SOURCE_DELIMITER).map((item) => {
+              return <p key={item}>{item}</p>
+            })}
+          </DetailList>
 
-      {additional !== '' && (
-        <DetailList>
-          <span>補充說明</span>
-          {additional.split(SOURCE_DELIMITER).map((item) => {
-            return <p key={item}>{item}</p>
-          })}
-        </DetailList>
+          {content !== '' && (
+            <DetailList>
+              <span>補充說明</span>
+              {content.split(SOURCE_DELIMITER).map((item) => {
+                return <p key={item}>{item}</p>
+              })}
+            </DetailList>
+          )}
+          <Sources sources={source} />
+        </>
       )}
-      <Sources sources={source} />
     </DetailContainer>
   )
 }
