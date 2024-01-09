47a4daf03c507bad4386d70ac6b4ab8908a700dc
Xuan <as840922qq@gmail.com>
2024-01-09 13:29:36 +0800

    fix(pt/landing2024): change scale to 350

--- packages/politics-tracker/components/landing/election-2024/fact-check-group/president-comparison/percentage-bar.tsx
+++ packages/politics-tracker/components/landing/election-2024/fact-check-group/president-comparison/percentage-bar.tsx
@@ -5,6 +5,9 @@ import Scale50 from '~/public/icons/landing/scale-50.svg'
 import Scale100 from '~/public/icons/landing/scale-100.svg'
 import Scale150 from '~/public/icons/landing/scale-150.svg'
 import Scale200 from '~/public/icons/landing/scale-200.svg'
+import Scale250 from '~/public/icons/landing/scale-250.svg'
+import Scale300 from '~/public/icons/landing/scale-300.svg'
+import Scale350 from '~/public/icons/landing/scale-350.svg'
 import type { CategoryOfJson } from '~/types/landing'
 
 const Wrapper = styled.div`
@@ -62,7 +65,7 @@ const ColorBar = styled.div`
 `
 
 const CategoryUnit = styled.div<{ count: number; color: string }>`
-  width: ${({ count }) => (count / 200) * 100 + '%' || '0%'};
+  width: ${({ count }) => (count / 350) * 100 + '%' || '0%'};
   height: 100%;
   background-color: ${({ color }) => color || '#fffff'};
   display: ${({ count }) => (count > 0 ? 'block' : 'none')};
@@ -101,6 +104,9 @@ export default function PercentageBar({
           <Scale100 />
           <Scale150 />
           <Scale200 />
+          <Scale250 />
+          <Scale300 />
+          <Scale350 />
         </ScaleBar>
       </BarContainer>
     </Wrapper>
