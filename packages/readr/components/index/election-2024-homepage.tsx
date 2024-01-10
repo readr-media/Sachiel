e6c26679af63a1f36f5cc72e07f239fd3e5ec948
DianYangFu <dianyangfu@gmail.com>
2024-01-10 13:38:22 +0800

    style(readr): adjust election-2024-homepage style

--- packages/readr/components/index/election-2024-homepage.tsx
+++ packages/readr/components/index/election-2024-homepage.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
   position: relative;
   padding: 32px 0;
-  background-color: #ebf02c;
+  background-color: #fff;
   iframe {
     margin: 0 auto;
     max-width: 1200px;
@@ -13,7 +13,7 @@ const Wrapper = styled.div`
 export default function Election2024Homepage() {
   return (
     <Wrapper>
-      <iframe src="https://www.readr.tw/project/3/dev-election2024-homepage-0110-6/index.html"></iframe>
+      <iframe src="https://www.readr.tw/project/3/dev-election2024-homepage-0110-7/index.html"></iframe>
     </Wrapper>
   )
 }
