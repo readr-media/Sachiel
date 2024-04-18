4cc251490cd4f15d6cb1aa58dd497e8e85b263da
caesar <caesar.lee@mirrormedia.mg>
2024-04-18 21:51:46 +0800

    refactor(mesh): update html lang for website

--- packages/mesh/app/layout.tsx
+++ packages/mesh/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh-Hant">
       <body>{children}</body>
     </html>
   )
