9c3497d8a404c0e64499ee486e913cdf3f5db7bc
caesar <caesar.lee@mirrormedia.mg>
2024-04-20 18:25:17 +0800

    feat(mesh): add apollo client
    - install apollo/client
    - install server-only
    - add apollo client factory function
    - add gql query example

---
+++ packages/mesh/apollo/index.ts
@@ -0,0 +1,25 @@
+import 'server-only'
+
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+
+import { API_ENDPOINT } from '@/constants/environment-variables'
+
+// reference: https://www.apollographql.com/blog/how-to-use-apollo-client-with-next-js-13
+// makes sure that we only instance the Apollo Client once per request,
+// since Apollo Client’s cache is designed with a single user in mind, we recommend that your Next.js server instantiates a new cache for each SSR request, rather than reusing the same long-lived instance for multiple users’ data.
+export const getClient = () => {
+  // creat a new client if there's no existing one
+  // or if we are running on the server.
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: API_ENDPOINT,
+    }),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+      },
+    },
+  })
+}
--- packages/mesh/app/page.tsx
+++ packages/mesh/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <main>Hello Mesh</main>
+import { gql } from '@apollo/client'
+
+import { getClient } from '@/apollo'
+
+export const revalidate = 60
+
+const fetchTest = gql`
+  query fetchCategoryBySlug {
+    allCategories(where: { slug: "nini_test_0410" }) {
+      name
+      slug
+      ogDescription
+    }
+  }
+`
+
+export default async function Home() {
+  const client = getClient()
+  const data = await client.query({
+    query: fetchTest,
+    context: {
+      fetchOptions: {
+        next: { revalidate: 0 },
+      },
+    },
+  })
+
+  const title = data.data.allCategories[0].ogDescription
+  return <main>Hello Mesh Title: {title}</main>
 }
