160a56bd5146871323c9d3aa2b835641e3977d7c
caesar <caesar.lee@mirrormedia.mg>
2024-05-02 19:22:19 +0800

    style(mesh): use theme method to reuse special unit

--- packages/mesh/app/layout.tsx
+++ packages/mesh/app/layout.tsx
@@ -19,29 +19,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-Hant" className={notoSans.className}>
-      <body className="min-h-screen pt-15 pb-16 sm:pl-32 sm:pt-16 sm:pb-0 md:pl-[220px] xl:pl-[calc((100vw-1440px)/2+320px)]">
-        <header className="fixed top-0 left-0 h-15 w-full border sm:h-16">
+      <body className="min-h-screen pt-15 pb-16 sm:pl-[theme(width.nav.sm)] sm:pt-16 sm:pb-0 md:pl-[theme(width.nav.md)] xl:pl-[calc((100vw-theme(width.maxContent))/2+theme(width.nav.xl))]">
+        <header className="fixed top-0 left-0 h-[theme(height.header.default)] w-full border sm:h-[theme(height.header.sm)]">
           {/* nested header to maintain the max width for screen width larger than 1440 */}
-          <div className="mx-auto h-full max-w-[1440px]">這是 header</div>
+          <div className="mx-auto h-full max-w-[theme(width.maxContent)]">
+            這是 header
+          </div>
         </header>
-        <main className="min-h-[calc(100%-641px)] w-full border sm:min-h-[calc(100dvh-317px-62px)]">
+        <main className="min-h-[calc(100dvh-theme(height.footer.default)-theme(height.header.default)-theme(height.nav.default))] w-full border sm:min-h-[calc(100dvh-theme(height.footer.sm)-theme(height.header.sm))]">
           {/* nested main to maintain the max width for screen width larger than 1440 */}
-          <div className="mr-auto max-w-[1220px]">{children}</div>
+          <div className="mr-auto max-w-[theme(width.maxMain)]">{children}</div>
         </main>
         {/*  */}
-        <footer className="h-[641px] border sm:h-[317px]">
+        <footer className="h-[theme(height.footer.default)] border sm:h-[theme(height.footer.sm)]">
           {/* nested footer to maintain the max width for screen width larger than 1440 */}
-          <div className="mr-auto h-full max-w-[1220px]">這是 footer</div>
+          <div className="mr-auto h-full max-w-[theme(width.maxMain)]">
+            這是 footer
+          </div>
         </footer>
         {/* fixed left nav shown on tablet, desktop size */}
-        <nav className="fixed left-0 top-16 hidden h-[calc(100%-64px)] w-32 border sm:block md:w-[220px] xl:w-[calc((100vw-1440px)/2+320px)] ">
+        <nav className="fixed left-0 top-16 hidden h-[calc(100%-theme(height.header.sm))] w-[theme(width.nav.sm)] border sm:block md:w-[theme(width.nav.md)] xl:w-[calc((100vw-theme(width.maxContent))/2+theme(width.nav.xl))] ">
           {/* nested nav bar to maintain the max width for screen width larger than 1440 */}
-          <div className="absolute top-0 right-0 h-full w-full xl:max-w-[320px]">
+          <div className="absolute top-0 right-0 h-full w-full xl:max-w-[theme(width.nav.xl)]">
             左側 navbar, 只在平板以上尺寸顯示
           </div>
         </nav>
         {/* fixed bottom nav bar shown on mobile only */}
-        <nav className="fixed bottom-0 left-0 h-16 w-full border sm:hidden">
+        <nav className="fixed bottom-0 left-0 h-[theme(height.nav.default)] w-full border sm:hidden">
           <div>置底 nav bar, 只在手機尺寸顯示</div>
         </nav>
       </body>
--- packages/mesh/tailwind.config.js
+++ packages/mesh/tailwind.config.js
@@ -13,8 +13,28 @@ module.exports = {
       xxl: '1920px',
     },
     extend: {
+      width: {
+        maxContent: '1440px',
+        maxMain: '1220px',
+        nav: {
+          sm: '128px',
+          md: '220px',
+          xl: '320px',
+        },
+      },
       height: {
         15: '3.75rem',
+        header: {
+          default: '60px',
+          sm: '64px',
+        },
+        footer: {
+          default: '641px',
+          sm: '317px',
+        },
+        nav: {
+          default: '64px',
+        },
       },
       minHeight: {
         screen: ['100vh /* fallback for Opera, IE and etc. */', '100dvh'],
