3c3a080268d719a396c6799d356ce73dbf014cc1
caesar <caesar.lee@mirrormedia.mg>
2024-05-07 15:49:37 +0800

    style(mesh): update layout style

--- packages/mesh/app/layout.tsx
+++ packages/mesh/app/layout.tsx
@@ -34,21 +34,21 @@ export default function RootLayout({
           {/* footer after main content */}
           <footer className="h-[theme(height.footer.default)] border-t bg-white sm:h-[theme(height.footer.sm)]">
             {/* nested footer to maintain the max width for screen width larger than 1440 */}
-            <div className="mr-auto h-full max-w-[theme(width.maxMain)]">
+            <div className="h-full max-w-[theme(width.maxMain)]">
               這是 footer
             </div>
           </footer>
         </div>
         {/* fixed left nav shown on tablet, desktop size */}
-        <nav className="fixed left-0 top-16 bottom-0 hidden w-[theme(width.nav.sm)] border-r bg-white sm:flex sm:justify-end md:w-[theme(width.nav.md)] xl:w-[calc((100vw-theme(width.maxContent))/2+theme(width.nav.xl))] ">
+        <nav className="fixed left-0 top-16 bottom-0 hidden w-[theme(width.nav.sm)] bg-white sm:flex sm:justify-end md:w-[theme(width.nav.md)] xl:w-[calc((100vw-theme(width.maxContent))/2+theme(width.nav.xl))] ">
           {/* nested nav bar to maintain the max width for screen width larger than 1440 */}
-          <div className="grow xl:max-w-[theme(width.nav.xl)]">
+          <div className="grow border-r xl:max-w-[theme(width.nav.xl)]">
             左側 navbar, 只在平板以上尺寸顯示
           </div>
         </nav>
         {/* fixed bottom nav bar shown on mobile only */}
         <nav className="fixed bottom-0 left-0 right-0 h-[theme(height.nav.default)] border-t bg-white sm:hidden">
-          <div>置底 nav bar, 只在手機尺寸顯示</div>
+          <div className="h-full">置底 nav bar, 只在手機尺寸顯示</div>
         </nav>
       </body>
     </html>
