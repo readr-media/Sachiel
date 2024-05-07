8a82c1c046725e18047d4af37d0b3923fb5c9039
“tyc” <yct.h89@gmail.com>
2024-05-07 19:14:33 +0800

    feat(mesh): update button styles

--- packages/mesh/components/button.tsx
+++ packages/mesh/components/button.tsx
@@ -2,15 +2,15 @@ import Icon, { type IconProps } from './icon'
 
 export default function Button({
   size,
-  bgColor,
+  color,
   text,
   icon,
   activeState,
   disable,
   onClick,
 }: {
-  size: keyof typeof sizeDefault
-  bgColor: keyof typeof colorDefault
+  size: keyof typeof buttonStyles
+  color: keyof typeof buttonColor
   text: string
   icon?: IconProps
   activeState?: {
@@ -21,22 +21,21 @@ export default function Button({
   disable?: boolean
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
 }) {
-  const colorDefault = {
+  const buttonColor = {
     white:
       'bg-white text-primary-700 border border-primary-700 hover:bg-primary-100 disabled:bg-disable disabled:text-primary-400 disabled:border-none disabled:cursor-not-allowed',
     blue: 'bg-custom-blue text-white disabled:bg-disable disabled:text-primary-400 disabled:cursor-not-allowed',
     primary:
       'bg-primary-700 text-white hover:bg-primary-800 disabled:bg-disable disabled:text-primary-400 disabled:cursor-not-allowed',
   }
-  const sizeDefault = {
-    xs: 'h-[30px] px-4 py-2 rounded-[100px]',
-    sm: 'h-8 px-3 py-1 rounded',
-    md: 'h-[38px] px-5 py-2 rounded',
-    'md-100': 'h-[38px] px-4 py-2 rounded-[100px]',
-    lg: 'w-full h-[46px] rounded',
+  const buttonStyles = {
+    xs: 'h-[30px] px-4 py-2 rounded-[100px] text-sm',
+    sm: 'h-8 px-3 py-1 rounded text-sm gap-0.5',
+    md: 'h-[38px] px-5 py-2 rounded text-base',
+    'md-100': 'h-[38px] px-4 py-2 rounded-[100px] text-base',
+    lg: 'w-full h-[46px] rounded text-base gap-1',
   }
 
-  const fontSize = size === 'xs' || size === 'sm' ? 'text-sm' : 'text-base'
   const {
     isActive = false,
     activeText = '',
@@ -45,22 +44,20 @@ export default function Button({
 
   return (
     <button
-      className={`flex ${sizeDefault[size]} items-center justify-center ${
-        isActive ? colorDefault['primary'] : colorDefault[bgColor]
+      className={`flex ${
+        buttonStyles[size]
+      } items-center justify-center font-normal ${
+        isActive ? buttonColor['primary'] : buttonColor[color]
       } `}
       onClick={onClick}
       disabled={disable}
     >
-      <span className="flex items-center">
-        {isActive && activeIcon ? (
-          <Icon {...activeIcon} />
-        ) : icon ? (
-          <Icon {...icon} />
-        ) : null}
-        <div className={`${fontSize} font-normal`}>
-          {isActive && activeText !== '' ? activeText : text}
-        </div>
-      </span>
+      {isActive && activeIcon ? (
+        <Icon {...activeIcon} />
+      ) : icon ? (
+        <Icon {...icon} />
+      ) : null}
+      {isActive && activeText !== '' ? activeText : text}
     </button>
   )
 }
