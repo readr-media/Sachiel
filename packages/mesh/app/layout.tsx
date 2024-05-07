c8b1cf19cbfed7c39e7281fd1be93f261ced990a
caesar <caesar.lee@mirrormedia.mg>
2024-05-07 11:54:43 +0800

    style(mesh): update layout style

--- packages/mesh/app/layout.tsx
+++ packages/mesh/app/layout.tsx
@@ -30,9 +30,7 @@ export default function RootLayout({
         {/* block for non-fixed content, set padding for fixed blocks */}
         <div className="flex min-h-screen flex-col bg-gray-50 pb-[theme(height.nav.default)] pt-[theme(height.header.default)]  sm:pb-0 sm:pt-[theme(height.header.sm)] sm:pl-[theme(width.nav.sm)] md:pl-[theme(width.nav.md)] xl:pl-[calc((100vw-theme(width.maxContent))/2+theme(width.nav.xl))]">
           {/* block for main and aside content to maintain the max width for screen width larger than 1440 */}
-          <div className="mr-auto w-full max-w-[theme(width.maxMain)] grow">
-            {children}
-          </div>
+          <div className="grow xl:max-w-[theme(width.maxMain)]">{children}</div>
           {/* footer after main content */}
           <footer className="h-[theme(height.footer.default)] border-t bg-white sm:h-[theme(height.footer.sm)]">
             {/* nested footer to maintain the max width for screen width larger than 1440 */}
