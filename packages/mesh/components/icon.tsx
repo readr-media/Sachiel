5371fee70d016aafee68c2e52d2af83b6953c783
caesar <caesar.lee@mirrormedia.mg>
2024-05-12 16:05:24 +0800

    refactor(mesh): extend icon component

--- packages/mesh/components/icon.tsx
+++ packages/mesh/components/icon.tsx
@@ -1,22 +1,45 @@
 import Image from 'next/image'
 
+type size = 's' | 'm' | 'l' | 'xl' | '2xl'
+type CustomSize = { width: number; height: number }
+
 export type IconProps = {
-  size: 's' | 'm' | 'l' | 'xl'
-  iconName: 'icon-dollar-white' | 'icon-star-primary' | 'icon-star-white'
+  size: size | CustomSize
+  iconName:
+    | 'icon-dollar-white'
+    | 'icon-star-primary'
+    | 'icon-star-white'
+    | 'icon-search'
+    | 'icon-notifications'
+    | 'icon-notifications-new'
+    | 'icon-search-bar'
+    | 'icon-readr-logo'
 }
 const sizeVariant = {
   s: 16,
   m: 20,
   l: 24,
   xl: 32,
+  '2xl': 44,
 }
 export default function Icon({ iconName, size }: IconProps) {
-  return (
-    <Image
-      src={`/icons/${iconName}.svg`}
-      width={sizeVariant[size]}
-      height={sizeVariant[size]}
-      alt={iconName}
-    />
-  )
+  if (typeof size === 'string') {
+    return (
+      <Image
+        src={`/icons/${iconName}.svg`}
+        width={sizeVariant[size]}
+        height={sizeVariant[size]}
+        alt={iconName}
+      />
+    )
+  } else {
+    return (
+      <Image
+        src={`/icons/${iconName}.svg`}
+        width={size.width}
+        height={size.height}
+        alt={iconName}
+      />
+    )
+  }
 }
