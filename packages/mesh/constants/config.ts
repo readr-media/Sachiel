dd04ef8964f2f25326890d73b529b7dfcb478e08
“tyc” <yct.h89@gmail.com>
2024-05-21 15:06:40 +0800

    feat(mesh/constants): add local api endpoint

--- packages/mesh/constants/config.ts
+++ packages/mesh/constants/config.ts
@@ -4,6 +4,8 @@ let API_ENDPOINT = ''
 
 switch (ENV) {
   case 'local':
+    API_ENDPOINT = 'https://mesh-gql-dev-4g6paft7cq-de.a.run.app/api/graphql'
+    break
   case 'dev':
     // use tv-cms api url for testing only
     API_ENDPOINT = 'https://api-dev.mnews.tw/admin/api'
