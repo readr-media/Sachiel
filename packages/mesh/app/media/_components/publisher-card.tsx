b45d2053ad6e79e8e756d64c92c897c9de0d3ff4
caesar <caesar.lee@mirrormedia.mg>
2024-06-18 17:13:32 +0800

    chore(mesh): add todo note

--- packages/mesh/app/media/_components/publisher-card.tsx
+++ packages/mesh/app/media/_components/publisher-card.tsx
@@ -54,6 +54,7 @@ export default function PublisherCard({
     <section className="rounded-lg bg-primary-100 px-5 py-2 lg:py-3 xl:px-8">
       <div className="flex h-[68px] items-center justify-between gap-1">
         <div className="flex gap-3">
+          {/* TODO: render logo of publisher */}
           <div>logo</div>
           <div>
             <div className="subtitle-2 text-primary-700">
