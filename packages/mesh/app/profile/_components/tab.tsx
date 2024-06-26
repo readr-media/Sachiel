54d839f243e7b6dfb9fbb1fbbba30566c1b3e5f3
LinHeMa <linhema0921@gmail.com>
2024-06-26 11:27:14 +0800

    feat(mesh/profile): tablet, pc layout

--- packages/mesh/app/profile/_components/tab.tsx
+++ packages/mesh/app/profile/_components/tab.tsx
@@ -18,24 +18,35 @@ const Tab = ({ category, setCategory }: TabProps) => {
     category === TabCategory.bookmarks ? activeTabStyle : inactiveTabStyle
   const picksActiveStyle =
     category === TabCategory.picks ? activeTabStyle : inactiveTabStyle
-
+  function handleTabOnClick(category: TabCategory) {
+    setCategory(category)
+  }
+  const tabList = [
+    {
+      name: '精選',
+      category: TabCategory.picks,
+      activeStyle: picksActiveStyle,
+    },
+    {
+      name: '書籤',
+      category: TabCategory.bookmarks,
+      activeStyle: bookmarksActiveStyle,
+    },
+  ]
   return (
     <ul
-      className="flex w-full items-center justify-around border-b border-t
-border-[rgba(0,9,40,0.1)]"
+      className="flex w-full items-center justify-around  border-b border-t border-[rgba(0,9,40,0.1)]
+sm:justify-start sm:gap-2 sm:pl-5 md:pl-[70px]"
     >
-      <li
-        onClick={() => setCategory(TabCategory.picks)}
-        className={`relative p-[14px] pt-3 font-normal leading-[22.4px] ${picksActiveStyle}`}
-      >
-        精選
-      </li>
-      <li
-        onClick={() => setCategory(TabCategory.bookmarks)}
-        className={`relative p-[14px] pt-3 font-normal leading-[22.4px] text-primary-400 ${bookmarksActiveStyle}`}
-      >
-        書籤
-      </li>
+      {tabList.map(({ name, category, activeStyle }) => (
+        <li
+          key={name}
+          onClick={() => handleTabOnClick(category)}
+          className={`relative p-[14px] pt-3 font-normal leading-[22.4px] ${activeStyle} sm:px-8 sm:py-[13px]`}
+        >
+          {name}
+        </li>
+      ))}
     </ul>
   )
 }
