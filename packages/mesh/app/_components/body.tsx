4043a4ae9b5b7f7355a9858d35ba5daca2a8f629
caesar <caesar.lee@mirrormedia.mg>
2024-07-03 14:05:19 +0800

    style(mesh): turn white page list into gray page list

--- packages/mesh/app/_components/body.tsx
+++ packages/mesh/app/_components/body.tsx
@@ -2,16 +2,16 @@
 
 import { usePathname } from 'next/navigation'
 
-import { BG_WHITE_PAGE_PATHS } from '@/constants/page-style'
+import { BG_GRAY_PAGE_PATHS } from '@/constants/page-style'
 
 export default function Body({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
-  const isBgWhite = !!BG_WHITE_PAGE_PATHS.find((path) =>
+  const isBgGray = !!BG_GRAY_PAGE_PATHS.find((path) =>
     pathname.startsWith(path)
   )
 
   return (
-    <body className={`min-h-screen ${isBgWhite ? 'bg-white' : 'bg-gray-50'}`}>
+    <body className={`min-h-screen ${isBgGray ? 'bg-gray-50' : 'bg-white'}`}>
       {children}
     </body>
   )
