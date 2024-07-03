2bc53cdd9c1b2587b7253a3a4e8aeecb51d0dc59
caesar <caesar.lee@mirrormedia.mg>
2024-07-03 13:29:15 +0800

    style(mesh): main content extend to full screen height

--- packages/mesh/app/social/_components/no-followings.tsx
+++ packages/mesh/app/social/_components/no-followings.tsx
@@ -8,7 +8,7 @@ export default async function NoFollowings() {
   const suggestedFollowers = await processMostFollowedMembers()
 
   return (
-    <main className="flex flex-col items-center justify-center gap-4 bg-white p-5 sm:bg-gray-50 lg:flex-row lg:items-start lg:justify-start lg:gap-10">
+    <main className="flex grow flex-col items-center justify-center gap-4 bg-white p-5 sm:bg-gray-50 lg:flex-row lg:items-start lg:justify-start lg:gap-10">
       <div className="flex w-full justify-center bg-white sm:max-w-[600px] sm:rounded-md sm:px-10 sm:py-15 sm:drop-shadow">
         <div className="flex flex-col gap-4">
           <div className="flex flex-col items-center gap-6">
