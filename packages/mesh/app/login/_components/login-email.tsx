cb4cedc9d90b1604838c88d8b2fe91901a36df4b
“tyc” <yct.h89@gmail.com>
2024-07-04 11:47:10 +0800

    refactor(login/login-email): remove isValidEmail extra condition

--- packages/mesh/app/login/_components/login-email.tsx
+++ packages/mesh/app/login/_components/login-email.tsx
@@ -72,5 +72,5 @@ function isValidEmail(email: string) {
   const emailRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
-  return email !== '' && emailRegex.test(email)
+  return emailRegex.test(email)
 }
