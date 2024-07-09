18e31798aadbc2bd5f083f1b04f270f641ce7d80
LinHeMa <linhema0921@gmail.com>
2024-07-09 17:07:26 +0800

    refactore(profile): change gql fetch method and style

--- packages/mesh/app/profile/_components/comment/comment.tsx
+++ packages/mesh/app/profile/_components/comment/comment.tsx
@@ -25,8 +25,12 @@ const Comment: React.FC<CommentProps> = ({
     cond: width > 960,
     route: '/story/',
   }
-  const { needClamp, commentRef, handleToggleClamp, defaultLineClamp } =
-    useCommentLogic(clampLineCount, canToggle, shouldRedirect)
+  const { needClamp, commentRef, handleToggleClamp } = useCommentLogic(
+    clampLineCount,
+    canToggle,
+    shouldRedirect
+  )
+  if (width < 960 && !data.content) return <></>
   return (
     <section className="mt-4 flex w-full flex-col gap-2 rounded-md border border-primary-200 bg-primary-100 p-3">
       <div className="flex items-center justify-between md:hidden">
@@ -44,6 +48,7 @@ const Comment: React.FC<CommentProps> = ({
             {displayTimeFromNow(data.createdAt)}
           </p>
           <Icon iconName="icon-dot" size="s" />
+
           <button className="caption-1 leading-[18px] text-primary-500">
             編輯留言
           </button>
@@ -58,12 +63,12 @@ const Comment: React.FC<CommentProps> = ({
         </div>
       </div>
       <div
-        className={`relative md:flex md:items-center ${
+        className={`relative md:flex md:items-start ${
           needClamp ? '' : 'after:opacity-0'
-        } after:body-3 after:absolute after:bottom-0 after:right-1 after:bg-gradient-to-r after:from-transparent after:from-0% after:to-primary-100 after:to-25% after:pl-6 after:leading-[21px] after:text-primary-400 after:content-['...繼續閱讀'] md:after:bottom-[3px]`}
+        } after:body-3 after:absolute after:bottom-0 after:right-1 after:bg-gradient-to-r after:from-transparent after:from-0% after:to-primary-100 after:to-25% after:pl-6 after:leading-[21px] after:text-primary-400 after:content-['...繼續閱讀'] md:after:bottom-[6px]`}
         onClick={handleToggleClamp}
       >
-        <div className="mr-2 hidden h-7 w-7 overflow-hidden rounded-full md:flex">
+        <div className="mr-2 hidden h-7 min-h-7 w-7 min-w-7 overflow-hidden rounded-full md:flex">
           <Image
             src={data.member?.avatar || avatar}
             width={28}
@@ -75,11 +80,6 @@ const Comment: React.FC<CommentProps> = ({
         <p
           className={`body-3 line-clamp-3 h-full w-full leading-[21px] text-primary-600 sm:line-clamp-1`}
           ref={commentRef}
-          style={{
-            overflow: 'hidden',
-            display: '-webkit-box',
-            WebkitLineClamp: defaultLineClamp,
-          }}
         >
           {data.content || '沒有評論'}
         </p>
