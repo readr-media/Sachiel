3025cac8cfd1339bef1bf951035979e765bcc105
“tyc” <yct.h89@gmail.com>
2024-07-11 10:05:07 +0800

    feat(hook/useAuthState): remove firebaseId state

--- packages/mesh/hooks/useAuthState.ts
+++ packages/mesh/hooks/useAuthState.ts
@@ -4,21 +4,18 @@ import { auth } from '@/firebase/client'
 
 export default function useAuthState() {
   const [isLogin, setIsLogin] = useState<boolean>(false)
-  const [firebaseId, setFirebaseId] = useState('')
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         setIsLogin(true)
-        setFirebaseId(authUser.uid)
       } else {
         setIsLogin(false)
-        setFirebaseId('')
       }
     })
 
     return () => unsubscribe()
   }, [])
 
-  return { isLogin, firebaseId }
+  return { isLogin }
 }
