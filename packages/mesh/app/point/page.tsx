7b89e0d403e6aa81c9ed7398a8a46e0dfa29245b
“tyc” <yct.h89@gmail.com>
2024-07-20 19:23:08 +0800

    feat(point/transaction-list): implement load more

--- packages/mesh/app/point/page.tsx
+++ packages/mesh/app/point/page.tsx
@@ -1,7 +1,7 @@
 import Icon from '@/components/icon'
 
-import LoadMoreTransaction from './_components/load-more-transaction'
 import MeshPointHelper from './_components/mesh-point-helper'
+import TransactionList from './_components/transaction-list'
 
 export default async function Page() {
   //TODO: update mock data to sync with server
@@ -13,7 +13,7 @@ export default async function Page() {
           id: 1,
           title: '廣告分潤',
           type: 'ad',
-          time: '2022/12/31 16:33',
+          time: '2024/12/31 16:33',
           amount: 1000,
         },
         {
@@ -21,21 +21,21 @@ export default async function Page() {
           title:
             '單篇訂閱鏡週刊 - 合法攻防還是政治惡鬥？那些年被癱瘓的國會：回顧十大爭議法案審查全記錄',
           type: 'sponsor',
-          time: '2022/5/19 06:29',
+          time: '2024/12/19 06:29',
           amount: -5,
         },
         {
           id: 3,
           title: '系統獎勵',
           type: 'reward',
-          time: '2022/12/31 16:33',
+          time: '2024/12/31 16:33',
           amount: 0.01,
         },
         {
           id: 4,
           title: '贊助 READr',
           type: 'sponsor',
-          time: '2022/5/19 06:29',
+          time: '2024/12/19 06:29',
           amount: -10,
         },
       ],
@@ -51,19 +51,78 @@ export default async function Page() {
           id: 5,
           title: '系統獎勵',
           type: 'reward',
-          time: '2022/10/20 10:00',
+          time: '2024/10/20 10:00',
           amount: 0.02,
+        },
+        {
+          id: 6,
+          title: '贊助報導者',
+          type: 'sponsor',
+          time: '2024/10/19 06:29',
+          amount: -10,
+        },
+      ],
+    },
+    {
+      month: 9,
+      record: [
+        {
+          id: 5,
+          title: '系統獎勵',
+          type: 'reward',
+          time: '2024/9/20 10:00',
+          amount: 0.02,
+        },
+        {
+          id: 6,
+          title:
+            '單篇訂閱鏡週刊 - CrowdStrike更新釀微軟當機 專家稱可能檢查不善',
+          type: 'sponsor',
+          time: '2024/9/19 06:29',
+          amount: -5,
+        },
+      ],
+    },
+    {
+      month: 8,
+      record: [
+        {
+          id: 5,
+          title: '系統獎勵',
+          type: 'reward',
+          time: '2024/8/20 10:00',
+          amount: 0.02,
+        },
+        {
+          id: 6,
+          title: '廣告分潤',
+          type: 'ad',
+          time: '2024/8/31 16:33',
+          amount: 0.5,
+        },
+      ],
+    },
+    {
+      month: 7,
+      record: [
+        {
+          id: 5,
+          title: '贊助鏡週刊',
+          type: 'sponsor',
+          time: '2024/7/20 10:00',
+          amount: -100000,
         },
         {
           id: 6,
           title: '首度連結錢包',
           type: 'reward',
-          time: '2022/10/15 14:20',
+          time: '2024/7/15 14:20',
           amount: 100,
         },
       ],
     },
   ]
+
   return (
     <main className="sm:p-5 md:px-[70px] md:py-10 lg:p-10">
       <div className="bg-white sm:rounded-md sm:drop-shadow">
@@ -93,61 +152,16 @@ export default async function Page() {
               <p className="body-3 text-primary-500">沒有交易紀錄</p>
             </section>
           ) : (
-            transactionData.map((data) => (
-              <section key={data.month} className="px-5 py-4 sm:px-10">
-                <h4 className="list-title pb-3 text-primary-700">
-                  {data.month}月
-                </h4>
-                {data.record.length === 0 ? (
-                  <p className="body-3 text-primary-500">沒有交易紀錄</p>
-                ) : (
-                  data.record.map((record, index) => (
-                    <div
-                      key={record.id}
-                      className={`flex flex-row gap-2 ${
-                        index === 0 ? 'pb-5' : 'py-5'
-                      } ${
-                        index !== data.record.length - 1
-                          ? 'border-b-[0.5px] border-primary-200'
-                          : ''
-                      }`}
-                    >
-                      <Icon
-                        iconName={
-                          record.type === 'sponsor'
-                            ? 'icon-publisher-readr'
-                            : 'icon-avatar-default'
-                        }
-                        size="2xl"
-                        className="h-11 w-11"
-                      />
-                      <div className="flex w-full flex-col gap-1">
-                        <div className="subtitle-2 flex justify-between gap-4">
-                          <p className=" text-primary-700">{record.title}</p>
-                          <p
-                            className={`${
-                              record.amount < 0
-                                ? 'text-primary-700'
-                                : 'text-custom-blue'
-                            }`}
-                          >
-                            {record.amount < 0
-                              ? record.amount
-                              : `+${record.amount}`}
-                          </p>
-                        </div>
-                        <p className="caption-1 text-primary-500">
-                          {record.time}
-                        </p>
-                      </div>
-                    </div>
-                  ))
-                )}
-              </section>
-            ))
+            <TransactionList
+              initialList={transactionData}
+              fetchMoreTransaction={async () => {
+                'use server'
+                //TODO: update fetching logic
+                return transactionData
+              }}
+            />
           )}
         </>
-        {transactionData.length === 0 ? null : <LoadMoreTransaction />}
       </div>
     </main>
   )
