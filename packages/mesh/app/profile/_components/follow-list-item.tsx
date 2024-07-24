faed73dab76d7f1da18c7e606bd1e2fb70083ebb
linHeMa <linhema0921@gmail.com>
2024-07-24 14:40:01 +0800

    refactor(follow): truncate

--- packages/mesh/app/profile/_components/follow-list-item.tsx
+++ packages/mesh/app/profile/_components/follow-list-item.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Link from 'next/link'
+
 import Button from '@/components/button'
 import Avatar from '@/components/story-card/avatar'
 
@@ -25,23 +27,28 @@ const FollowListItem = ({
       key={followerId}
       className="flex items-center justify-between pb-5 pt-[12.5px] first-of-type:pt-[24.5px] lg:px-5"
     >
-      <span className="flex items-center">
-        <Avatar src={followerAvatar} size="l" extra="mr-2" />
-        <div className="flex flex-col">
-          <p className="subtitle-1">{followerName}</p>
-          <p className="body-3 text-primary-500">{followerCustomId}</p>
+      <Link
+        href={`/profile/member/${followerCustomId}`}
+        className="flex w-full grow items-center"
+      >
+        <Avatar src={followerAvatar} size="l" extra="mr-2 shrink-0" />
+        <div className="mr-2 flex min-w-0 flex-grow flex-col">
+          <p className="subtitle-1 truncate">{followerName}</p>
+          <p className="body-3 truncate text-primary-500">{followerCustomId}</p>
         </div>
-      </span>
-      <Button
-        color="white"
-        onClick={handleFollowOnClick}
-        size="sm"
-        text="追蹤"
-        activeState={{
-          isActive: true,
-          activeText: '追蹤中',
-        }}
-      />
+        <div className="shrink-0">
+          <Button
+            color="white"
+            onClick={handleFollowOnClick}
+            size="sm"
+            text="追蹤"
+            activeState={{
+              isActive: true,
+              activeText: '追蹤中',
+            }}
+          />
+        </div>
+      </Link>
     </li>
   )
 }
