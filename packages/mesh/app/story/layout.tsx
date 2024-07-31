64256b5d4cad139efb7c592127651c79336cec5d
caesar <caesar.lee@mirrormedia.mg>
2024-07-31 15:23:28 +0800

    style(mesh/story): remove unnecessary style

--- packages/mesh/app/story/layout.tsx
+++ packages/mesh/app/story/layout.tsx
@@ -15,7 +15,7 @@ export default function MediaLayout({
       <div className="primary-container-article">
         {/* block for main and aside content to maintain the max width for screen width larger than 1440 */}
         <div className="flex grow flex-col items-center bg-white">
-          <div className="relative flex w-full grow justify-around xl:max-w-[theme(width.maxContent)]">
+          <div className="flex w-full grow justify-around xl:max-w-[theme(width.maxContent)]">
             {children}
           </div>
         </div>
