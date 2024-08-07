575cdbafc91cae8c7243091f323fad306cbf916d
caesar <caesar.lee@mirrormedia.mg>
2024-08-07 14:08:14 +0800

    refactor(readr): update comscore

--- packages/readr/components/comscore-script.js
+++ packages/readr/components/comscore-script.js
@@ -1,19 +1,21 @@
+import Head from 'next/head'
+
 /**
  * Component for implement comScore script.
  */
 export default function ComScoreScript() {
   return (
     // use react script rather than next/Script to let the script show on the source of the html (view-source:)
-    <>
+    <Head>
       <script
         id="comScore"
         dangerouslySetInnerHTML={{
           __html: `
             var _comscore = _comscore || [];
-            _comscore.push({ c1: "2”, c2: "24318560" ,  options: { enableFirstPartyCookie: "true” } });
+            _comscore.push({ c1: "2", c2: "24318560" ,  options: { enableFirstPartyCookie: "true" } });
             (function() {
-              var s = document.createElement("script”), el = document.getElementsByTagName("script”)[0]; s.async = true;
-              s.src = "https://sb.scorecardresearch.com/cs/24318560/beacon.js”;
+              var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
+              s.src = "https://sb.scorecardresearch.com/cs/24318560/beacon.js";
               el.parentNode.insertBefore(s, el);
             })();
           `,
@@ -21,9 +23,9 @@ export default function ComScoreScript() {
       />
       <noscript
         dangerouslySetInnerHTML={{
-          __html: `<img src="https://sb.scorecardresearch.com/p?c1=2&amp;c2=24318560&amp;cv=3.9.1&amp;cj=1”>`,
+          __html: `<img src="https://sb.scorecardresearch.com/p?c1=2&amp;c2=24318560&amp;cv=3.9.1&amp;cj=1">`,
         }}
       />
-    </>
+    </Head>
   )
 }
