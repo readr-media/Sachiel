b73ae012fbbda2fd30bda25ddb96c123fc27c91d
linhema <linhema0921@gmail.com>
2024-08-21 15:22:10 +0800

    fix(edit-profile): block not user entring edit page

--- packages/mesh/app/profile/member/[customId]/edit-profile/layout.tsx
+++ packages/mesh/app/profile/member/[customId]/edit-profile/layout.tsx
@@ -1,10 +1,19 @@
+'use client'
+import { useParams, useRouter } from 'next/navigation'
+
 import EditProfileHeader from '@/app/profile/_components/edit-profile-header'
+import { useUser } from '@/context/user'
 
 const EditProfileLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) => {
+  const { user } = useUser()
+  const params = useParams()
+  const router = useRouter()
+  const isUser = params.customId === user.customId
+  if (!isUser) router.push(`/profile/member/${params.customId}`)
   return (
     <>
       <EditProfileHeader title="編輯個人檔案" />
