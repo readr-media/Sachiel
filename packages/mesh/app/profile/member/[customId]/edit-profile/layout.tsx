83e99c1471dc5468a706039a3832d3581cb403db
linhema <linhema0921@gmail.com>
2024-08-22 16:52:05 +0800

    style(edit-profile): layout UI

--- packages/mesh/app/profile/member/[customId]/edit-profile/layout.tsx
+++ packages/mesh/app/profile/member/[customId]/edit-profile/layout.tsx
@@ -2,6 +2,8 @@
 import { useParams, useRouter } from 'next/navigation'
 
 import EditProfileHeader from '@/app/profile/_components/edit-profile-header'
+import LayoutTemplate from '@/components/layout-template'
+import { useEditProfile } from '@/context/edit-profile'
 import { useUser } from '@/context/user'
 
 const EditProfileLayout = ({
@@ -12,13 +14,47 @@ const EditProfileLayout = ({
   const { user } = useUser()
   const params = useParams()
   const router = useRouter()
+  const title = '編輯個人檔案'
+  const backToPreviousPage = () => {
+    router.back()
+  }
   const isUser = params.customId === user.customId
   if (!isUser) router.push(`/profile/member/${params.customId}`)
+  const { handleSubmit, isFormValid } = useEditProfile()
   return (
-    <>
-      <EditProfileHeader title="編輯個人檔案" />
+    <LayoutTemplate
+      type="default"
+      customStyle={{
+        background: 'bg-multi-layer-light',
+        footer: 'hidden sm:block',
+        nav: 'hidden sm:block',
+      }}
+      navigation={{
+        leftButtons: [
+          {
+            type: 'text',
+            text: '取消',
+            color: 'gray',
+            onClick: backToPreviousPage,
+          },
+        ],
+        title,
+        rightButtons: [
+          {
+            type: 'text',
+            text: '儲存',
+            color: isFormValid ? 'blue' : 'gray',
+            onClick: handleSubmit,
+          },
+        ],
+      }}
+    >
+      <EditProfileHeader
+        title={title}
+        backToPreviousPage={backToPreviousPage}
+      />
       {children}
-    </>
+    </LayoutTemplate>
   )
 }
 
