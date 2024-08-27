67381fee04dd32794f7783ff1ec644c2c8ccb747
linhema <linhema0921@gmail.com>
2024-08-27 16:41:13 +0800

    fix(error-status): remove error digest

--- packages/mesh/app/global-error.tsx
+++ packages/mesh/app/global-error.tsx
@@ -3,19 +3,17 @@
 import ErrorPage from '@/components/status/error-page'
 
 export default function GlobalError({
-  error,
+  error: _error,
   reset,
 }: {
   error: Error & { digest?: string }
   reset: () => void
 }) {
-  const statusCode = error.digest?.includes('NEXT_NOT_FOUND') ? 404 : 500
-  // TODO: cannot add stateful header
   return (
     <html>
       <body className="flex min-h-screen flex-col">
         <div className="flex grow flex-col items-center justify-center">
-          <ErrorPage statusCode={statusCode} reset={reset} />
+          <ErrorPage statusCode={500} reset={reset} />
         </div>
       </body>
     </html>
