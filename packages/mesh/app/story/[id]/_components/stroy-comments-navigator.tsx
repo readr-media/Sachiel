80f4a1f4381edffd19b6752c9cf83a7b6d0f3bd4
LinHeMa <linhema0921@gmail.com>
2024-09-05 19:14:33 +0800

    tmp

--- packages/mesh/app/story/[id]/_components/stroy-comments-navigator.tsx
+++ packages/mesh/app/story/[id]/_components/stroy-comments-navigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const StoryCommentsNavigator = () => {
-  return <div>StoryCommentsNavigator</div>
+  return <div>test in ipad oasis</div>
 }
 
 export default StoryCommentsNavigator
