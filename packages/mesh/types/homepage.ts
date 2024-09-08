f3ef976bbfb73575ea7571950f495a3907e484ea
nihclil <oxx921@gmail.com>
2024-09-09 04:24:28 +0800

    feat: add homepage related types

---
+++ packages/mesh/types/homepage.ts
@@ -0,0 +1,77 @@
+type Member = { id: string; name: string; avatar: string }
+
+type Source = { id: string; title: string; customId: string }
+
+export type Story = {
+  id: string
+  url: string
+  title: string
+  published_date: string
+  summary: string
+  og_title: string
+  og_image: string
+  og_description: string
+  full_content: boolean
+  commentCount: number
+  paywall: boolean
+  full_screen_ad: string
+  isMember: boolean
+  pickCount: number
+  picks: {
+    createdAt?: string
+    member: Member | null
+  }[]
+}
+
+export type MostPickedStory = Story & { source: Source }
+
+export type ReadrStory = Source & {
+  stories: Story[]
+}
+//TODO: null member
+export type DailyStory = Omit<
+  Story,
+  'isMember' | 'full_screen_ad' | 'summary'
+> & {
+  source: Source
+  category: {
+    slug: string
+  }
+}
+
+export type SponsoredStory = Source & {
+  stories: Omit<Story, 'picks' | 'pickCount'>[]
+  sponsoredCount: number
+}
+// TODO: null story
+export type Comment = {
+  member: Member
+  id: string
+  content: string
+  likeCount: number
+  story: {
+    id: string
+    title: string
+    published_date: string
+    source: Source
+  } | null
+}
+
+export type Collector = {
+  id: number
+  name: string
+  avatar: string
+  nickname: string
+  email: string
+  pickCount: number
+}
+
+export type CategoryStory = Omit<Story, 'isMember' | 'pickCount'> & {
+  picksCount: number
+  content: string
+  category: {
+    id: string
+    slug: string
+  }
+  source: Omit<Source, 'customId'>
+}
