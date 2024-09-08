3ca6b4dd24f6ce658f1e07e8bc9c77e7bd58d521
nihclil <oxx921@gmail.com>
2024-09-09 04:30:30 +0800

    feat: add story card

---
+++ packages/mesh/app/_component/story-card.tsx
@@ -0,0 +1,59 @@
+import NextImage from 'next/image'
+import NextLink from 'next/link'
+
+import StoryMeta from '@/components/story-card/story-meta'
+import StoryPickButton from '@/components/story-card/story-pick-button'
+import StoryPickInfo from '@/components/story-card/story-pick-info'
+import type { CategoryStory, DailyStory } from '@/types/homepage'
+import { getDisplayPicks } from '@/utils/story-display'
+
+type Props<T> = {
+  story: T
+  followingMembers: Set<string>
+}
+
+export default function StoryCard<T extends CategoryStory | DailyStory>({
+  story,
+  followingMembers,
+}: Props<T>) {
+  const displayPicks = getDisplayPicks(story.picks, followingMembers)
+
+  return (
+    <div className="pb-4 pt-5 shadow-[0_0.5px_0_0_rgba(0,9,40,0.1)]">
+      <p className="caption-1 mb-1 text-primary-500">
+        {/* TODO: customId */}
+        <NextLink href={`/`}>{story.source.title}</NextLink>
+      </p>
+      <div className="flex justify-between gap-x-3 sm:gap-x-10">
+        <div>
+          <p className="subtitle-1 sm:title-2 mb-2 line-clamp-2 grow text-primary-700 sm:mb-1">
+            <NextLink href={`/story/${story.id}`}> {story.title}</NextLink>
+          </p>
+          <div className="caption-1">
+            {/* TODO: fullScreenAd */}
+            <StoryMeta
+              commentCount={story.commentCount}
+              publishDate={story.published_date}
+              paywall={story.paywall}
+              fullScreenAd=""
+            />
+          </div>
+        </div>
+
+        <NextLink href={`/story/${story.id}`}>
+          <div className="relative h-[48px] w-[96px] shrink-0 overflow-hidden rounded sm:h-[80px] sm:w-[160px]">
+            <NextImage src={story.og_image} fill alt={story.title} />
+          </div>
+        </NextLink>
+      </div>
+
+      <div className="mt-4 flex justify-between">
+        <StoryPickInfo
+          displayPicks={displayPicks}
+          pickCount={'pickCount' in story ? story.pickCount : story.picksCount}
+        />
+        <StoryPickButton storyId={story.id} />
+      </div>
+    </div>
+  )
+}
