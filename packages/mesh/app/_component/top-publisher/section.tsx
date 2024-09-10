681a516fd3d1b5c6861b9becaa87aba5f5998d4b
nihclil <oxx921@gmail.com>
2024-09-10 23:08:10 +0800

    styles: adjust padding

--- packages/mesh/app/_component/top-publisher/section.tsx
+++ packages/mesh/app/_component/top-publisher/section.tsx
@@ -7,7 +7,7 @@ export default async function TopPublisherSection() {
   if (!data) return null
 
   return (
-    <section className="px-5 pb-10 pt-4 sm:pb-[22px] md:px-[70px] lg:px-10 lg:pb-15 lg:pt-8">
+    <section className="px-5 pb-10 pt-8 sm:pb-[22px] md:px-[70px] lg:px-10 lg:pb-15 lg:pt-10 xxl:pb-[43px]">
       <h2 className="list-title lg:title-1 mb-3 text-primary-700 lg:mb-4">
         獲得最多次贊助
       </h2>
