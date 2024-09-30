fab00b55d9150c79cd50f1e32bdbfdc82cb8532a
YuCheTeng <114332515+tyc67@users.noreply.github.com>
2024-09-30 13:55:10 +0800

    fix: alchemy address

--- packages/mesh/app/alchemyhandler/page.tsx
+++ packages/mesh/app/alchemyhandler/page.tsx
@@ -1,20 +1,42 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useAccount, useSignerStatus } from '@alchemy/aa-alchemy/react'
+import { notFound, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 import Spinner from '@/components/spinner'
+import { useUser } from '@/context/user'
+import { accountType } from '@/utils/alchemy'
+
+import { updateMemberWallet } from '../actions/auth'
 
 // handle redirects from alchemy email link to user interacting page
 export default function Page() {
   const router = useRouter()
+  const { isConnected } = useSignerStatus()
+  const { address } = useAccount({ type: accountType })
+  const { user } = useUser()
+  const hasAlchemyAccount = !!user.wallet
+  const memberId = user.memberId
+
+  if (!memberId) notFound()
 
   useEffect(() => {
-    const route = localStorage.getItem('alchemy-redirect')
-    if (route) {
-      router.push(route)
+    const handleAlchemyRedirect = async () => {
+      if (!isConnected || !address) return
+
+      const route = localStorage.getItem('alchemy-redirect')
+      if (!route) return
+
+      if (hasAlchemyAccount) {
+        router.push(route)
+      } else {
+        const response = await updateMemberWallet(memberId, address)
+        if (response) router.push(route)
+      }
     }
-  }, [router])
+    handleAlchemyRedirect()
+  }, [address, hasAlchemyAccount, isConnected, memberId, router])
 
   return (
     <body>
