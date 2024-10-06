e893856707a05e58cac0b5a531f115a61ea38f0a
nihclil <oxx921@gmail.com>
2024-10-07 04:05:05 +0800

    feat: add endpoint

--- packages/mesh/constants/config.ts
+++ packages/mesh/constants/config.ts
@@ -73,8 +73,10 @@ const STATIC_FILE_ENDPOINTS = {
   mostLikeComments: `${STATIC_FILE_ORIGIN}/data/hotpage_most_like_comments.json`,
   dailyHighlightGroup: `${STATIC_FILE_ORIGIN}/data/hotpage_group.json`,
   dailyHighlightNoGroup: `${STATIC_FILE_ORIGIN}/data/hotpage_no_group.json`,
-  groupStoriesInCategeoryfn: (categoryName: string) =>
+  groupAndOtherStoriesInCategeoryfn: (categoryName: string) =>
     `${STATIC_FILE_ORIGIN}/data/group_${categoryName}.json`,
+  categoryMostSponsoredPublishersfn: (categoryName: string) =>
+    `${STATIC_FILE_ORIGIN}/data/${categoryName}_recommend_sponsors.json`,
 }
 
 export {
