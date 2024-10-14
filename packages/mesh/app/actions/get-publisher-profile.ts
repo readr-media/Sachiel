b471c7448deebd339ebbc1543c5fcf28e5b6421c
LinHeMa <linhema0921@gmail.com>
2024-10-14 09:58:59 +0800

    refactor(profile): type typo

--- packages/mesh/app/actions/get-publisher-profile.ts
+++ packages/mesh/app/actions/get-publisher-profile.ts
@@ -3,14 +3,14 @@ import type { Story } from '@/graphql/__generated__/graphql'
 import fetchStatic from '@/utils/fetch-static'
 import { getLogTraceObjectFromHeaders } from '@/utils/log'
 
-type publisherStoriesFnType = {
+type PublisherStoriesFnType = {
   source: { id: string; title: string; official_site: string }
   stories: Story[]
 }
 
 export const publisherStoriesFn = async (publisherCustomId: string) => {
   const globalLogFields = getLogTraceObjectFromHeaders()
-  return fetchStatic<publisherStoriesFnType>(
+  return fetchStatic<PublisherStoriesFnType>(
     STATIC_FILE_ENDPOINTS.publisherStoriesFn(publisherCustomId),
     { next: { revalidate: 10 } },
     globalLogFields
