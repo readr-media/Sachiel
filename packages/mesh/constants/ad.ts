a13b3b6d4503d4ddd76d46a9445722a7b69b6704
nihclil <oxx921@gmail.com>
2025-01-20 15:49:04 +0800

    feat: add aside ad

--- packages/mesh/constants/ad.ts
+++ packages/mesh/constants/ad.ts
@@ -7,5 +7,15 @@ export const ADSENSE_UNITS: AdsenseUnits = {
       adSlot: '5137587701',
       adSize: [728, 90],
     },
+    PC_D3: {
+      adUnit: 'Mesh_pc_media_160x600_D3',
+      adSlot: '8937571223',
+      adSize: [160, 600],
+    },
+    MB_MD1: {
+      adUnit: 'Mesh_m_media_300x250_MD1',
+      adSlot: '8760443410',
+      adSize: [300, 250],
+    },
   },
 }
