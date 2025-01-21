bfd7019c39ade0c9c5ad1a1a5464cacea6a930de
nihclil <oxx921@gmail.com>
2025-01-21 11:30:21 +0800

    feat: insert ads between desktop stories on media page

--- packages/mesh/constants/ad.ts
+++ packages/mesh/constants/ad.ts
@@ -12,7 +12,32 @@ export const ADSENSE_UNITS: AdsenseUnits = {
       adSlot: '8937571223',
       adSize: [160, 600],
     },
-    MB_MD1: {
+    'PC_D2-1': {
+      adUnit: 'Mesh_pc_media_728x90_D2-1',
+      adSlot: '2281567494',
+      adSize: [600, 90],
+    },
+    'PC_D2-2': {
+      adUnit: 'Mesh_pc_media_728x90_D2-2',
+      adSlot: '8183232069',
+      adSize: [600, 90],
+    },
+    'PC_D2-3': {
+      adUnit: 'Mesh_pc_media_728x90_D2-3',
+      adSlot: '3588841933',
+      adSize: [600, 90],
+    },
+    'PC_D2-4': {
+      adUnit: 'Mesh_pc_media_728x90_D2-4',
+      adSlot: '2275760268',
+      adSize: [600, 90],
+    },
+    'PC_D2-5': {
+      adUnit: 'Mesh_pc_media_728x90_D2-5',
+      adSlot: '1006615475',
+      adSize: [600, 90],
+    },
+    'MB_MD2-1': {
       adUnit: 'Mesh_m_media_300x250_MD1',
       adSlot: '8760443410',
       adSize: [300, 250],
