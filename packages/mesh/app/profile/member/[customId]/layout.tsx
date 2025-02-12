c4dac2704f37f907a05b58452bd7e65908538147
caesar <caesar.lee@mirrormedia.mg>
2025-02-12 13:55:50 +0800

    refactor(mesh): update profile og:image

--- packages/mesh/app/profile/member/[customId]/layout.tsx
+++ packages/mesh/app/profile/member/[customId]/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 
-import { getMemberName } from '@/app/actions/get-profile'
+import { getMemberForOG } from '@/app/actions/get-profile'
 import { metadata as rootMetadata } from '@/app/layout'
-import { SITE_TITLE, SITE_URL } from '@/constants/config'
+import { SITE_OG_IMAGE, SITE_TITLE, SITE_URL } from '@/constants/config'
 
 import ClientLayout from './_components/client-layout'
 
@@ -13,8 +13,12 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const memberCustomId = params.customId
 
-  const memberData = await getMemberName(memberCustomId)
+  const memberData = await getMemberForOG(memberCustomId)
   const memberName = memberData?.member?.name
+  const memberAvatar =
+    memberData?.member?.avatar ||
+    memberData?.member?.avatar_image?.resized?.original ||
+    SITE_OG_IMAGE
 
   const metaTitle = memberName ? `${memberName} | ${SITE_TITLE}` : SITE_TITLE
   const metaDescription = memberName
@@ -30,6 +34,9 @@ export async function generateMetadata({
       url: SITE_URL + `/profile/member/${memberCustomId}`,
       title: metaTitle,
       description: metaDescription,
+      images: {
+        url: memberAvatar,
+      },
     },
   }
 }
