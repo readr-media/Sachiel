b10587b91e6a980aea06b0ca671f8b41e05cf907
caesar <caesar.lee@mirrormedia.mg>
2025-03-19 15:46:47 +0800

    fix(mesh): notFound error fall into this route

--- packages/mesh/app/[slug]/layout.tsx
+++ packages/mesh/app/[slug]/layout.tsx
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+import { notFound } from 'next/navigation'
+
+>>>>>>> d7a33928 (fix(mesh): notFound error fall into this route)
 import LayoutTemplate from '@/components/layout-template'
 import GoBackButton from '@/components/navigation/go-back-button'
 
@@ -11,11 +16,19 @@ export default async function SubpageLayout({
   params: { slug: string }
 }) {
   const categorySlug = params.slug
+<<<<<<< HEAD
   const result = await fetchCategoryInformation(categorySlug)
   const title =
     categorySlug !== 'podcast'
       ? `${result?.title}熱門`
       : `${result?.title} 熱門`
+=======
+  const slugInfo = await fetchCategoryInformation(categorySlug)
+
+  if (!slugInfo) notFound()
+
+  const title = `${slugInfo.title}熱門`
+>>>>>>> d7a33928 (fix(mesh): notFound error fall into this route)
 
   const navigationData = {
     title,
