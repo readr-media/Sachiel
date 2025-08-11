d36d7421fe2a5087b185e3893f702146657e045f
LinHeMa <linhema0921@gmail.com>
2025-08-11 16:27:15 +0800

    fix: middle image matcher

--- packages/mesh/middleware.ts
+++ packages/mesh/middleware.ts
@@ -76,5 +76,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|icons).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico|icons).*)'],
 }
