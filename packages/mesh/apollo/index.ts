51cb82e51ad59b9002d0ddc408d79a1a2b170fe8
hcchien <hcchien@gmail.com>
2025-10-08 12:11:08 +0800

    fix: revert apollo client

--- packages/mesh/apollo/index.ts
+++ packages/mesh/apollo/index.ts
@@ -1,9 +1,7 @@
 import 'server-only'
 
 import type { ApolloLink } from '@apollo/client'
-import { ApolloClient, from, InMemoryCache, split } from '@apollo/client'
-import { BatchHttpLink } from '@apollo/client/link/batch-http'
-import { cache } from 'react'
+import { ApolloClient, from, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { onError } from '@apollo/client/link/error'
 import createUploadLink from 'apollo-upload-client/createUploadLink.mjs'
@@ -39,46 +37,21 @@ const uploadLink = createUploadLink({
   },
 }) as unknown as ApolloLink
 
-const batchLink = new BatchHttpLink({
-  uri: GQL_ENDPOINT,
-  batchMax: 10,
-  batchInterval: 10, // ms
-  headers: {
-    'Apollo-Require-Preflight': 'true',
-  },
-}) as unknown as ApolloLink
+// no batch link in the previous stable version
 
 // reference: https://www.apollographql.com/blog/how-to-use-apollo-client-with-next-js-13
 // makes sure that we only instance the Apollo Client once per request,
 // since Apollo Client’s cache is designed with a single user in mind, we recommend that your Next.js server instantiates a new cache for each SSR request, rather than reusing the same long-lived instance for multiple users’ data.
-export const getClient = cache(() => {
-  // create a new client once per request with React cache
-  const uploadOperationNames = new Set(['CreatePhoto'])
-
-  const link = from([
-    errorLink,
-    authLink,
-    // 依據是否為檔案上傳分流：上傳走 uploadLink，否則走批次連結
-    split(
-      (operation) => uploadOperationNames.has(operation.operationName || ''),
-      uploadLink,
-      batchLink
-    ),
-  ])
-
+export const getClient = () => {
+  // previous stable version: simple link chain without batching or per-request cache wrapper
   return new ApolloClient({
-    link,
+    link: from([errorLink, authLink, uploadLink]),
     cache: new InMemoryCache(),
-    queryDeduplication: true,
     defaultOptions: {
-      watchQuery: {
-        fetchPolicy: 'cache-and-network',
-        errorPolicy: 'all',
-      },
       query: {
-        fetchPolicy: 'cache-first',
+        fetchPolicy: 'no-cache',
         errorPolicy: 'all',
       },
     },
   })
-})
+}
